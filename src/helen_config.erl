5644dde5bd9cccfa1f394e4f9048d63766dffa56
Bryan Fink <bfink@vmware.com>
2018-02-20 15:17:53 -0800

    routing for Angular app
    Angular one-page apps require their base HTML to be served from every
    URL they use. The choice of what to display is made on the browser
    side. This commit changes routing such that:
    
     * priv/www/index.html is served for any request that does not
       dispatch to another URL
    
     * /api/... URLs are the one exception to the above rule - undefined
       sub-paths here serve a 404 status
    
     * priv/www/assets/ is served at /assets/, and should become the place
       for UI support files
    
     * priv/www/swagger continues to be served at /swagger, but the
       routing is now explicit

--- src/helen_config.erl
+++ src/helen_config.erl
@@ -11,12 +11,24 @@
 
 -spec dispatch() -> [webmachine_dispatcher:route()].
 dispatch() ->
+    Priv = code:priv_dir(helen),
     lists:flatten([
         {["api"], helen_api_base_resource, []},
         {["api", "athena", "members"], helen_api_member_list_resource, []},
         {["api", "athena", "eth"], helen_api_eth_resource, []},
-        %% serve everything in priv/www, at URLs matching the paths
-        {['*'], helen_static_resource, []}
+        %% enforce that we don't serve the Angular HTML at undefined /api URLs
+        {["api", '*'], helen_404_resource, []},
+
+        {["swagger", '*'], helen_static_resource,
+         [{dir, filename:join([Priv, "www", "swagger"])}]},
+        {["assets", '*'], helen_static_resource,
+         [{dir, filename:join([Priv, "www", "assets"])}]},
+
+        %% Everything else will load the single-page Angular app. It
+        %% needs every URL to serve its base HTML. It decides which
+        %% page to render, on the browser side, based on the URL.
+        {['*'], helen_static_resource,
+         [{file, filename:join([Priv, "www", "index.html"])}]}
     ]).
 
 web_config() ->
