16b59b453d8b207b3585e6ad86890900d7fa647a
Bryan Fink <bfink@vmware.com>
2018-02-23 18:48:27 -0800

    first roundtrip from helen works
    except that we hit a doublefree right after responding (TODO)

--- README.md
+++ README.md
@@ -16,6 +16,28 @@ The project is composed of three components:
 
  * (helen)[vmathena/helen] is the home of Athena's UI and external API
 
+## Building
+
+You will need Boost installed. Version 1.64.0 has been tested (note
+the addition of `program_options` if you had built Boost for the
+`P2_Blockchain` project already:
+
+```
+wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz
+tar -xzf boost_1_64_0.tar.gz
+cd boost_1_64_0
+./bootstrap.sh --with-libraries=system,filesystem,program_options --prefix=/usr
+./b2
+sudo ./b2 install
+```
+
+This project uses protocol buffers, so you'll need both the protoc
+compile and the C++ headers. On Ubunutu, you can get these via apt:
+
+```
+$ sudo apt-get install libprotobuf-dev protobuf-compiler
+```
+
 TODO: implement the equivalent of
 P2_Blockchain/Blockchain/basicBlockchain2 and
 P2_Blockchain/Blockchan/EtheriumBlockchain here.
--- src/athena_api_connection.cpp
+++ src/athena_api_connection.cpp
@@ -2,11 +2,18 @@
 //
 // Acceptor for connections from the API/UI servers.
 
+#include <iostream>
 #include <boost/bind.hpp>
+#include <boost/predef/detail/endian_compat.h>
 
 #include "athena_api_connection.hpp"
+#include "athena.pb.h"
 
 using boost::asio::ip::tcp;
+using com::vmware::athena::AthenaRequest;
+using com::vmware::athena::AthenaRequest_TestRequest;
+using com::vmware::athena::AthenaResponse;
+using com::vmware::athena::AthenaResponse_TestResponse;
 
 athena_api_connection::pointer
 athena_api_connection::create(boost::asio::io_service &io_service)
@@ -23,6 +30,55 @@ athena_api_connection::socket()
 void
 athena_api_connection::start()
 {
+    uint16_t msglen;
+    char msg[65536];
+    std::string pb;
+    boost::asio::mutable_buffer lengthbuf(&msglen, sizeof(msglen));
+    boost::asio::mutable_buffer msgbuf(&msg, 65536);
+    boost::system::error_code error;
+    
+    while (boost::asio::read(socket_, boost::asio::buffer(lengthbuf), error)) {
+#ifndef BOOST_LITTLE_ENDIAN
+        // swap byte order for big endian and pdp endian
+        msglen = (msglen << 8) | (msglen >> 8)
+#endif
+        std::cout << "Bytes read! expecting " << msglen << " more" << std::endl;
+
+        if (boost::asio::read(socket_, boost::asio::buffer(msgbuf),
+                              boost::asio::transfer_at_least(msglen), error)
+            == msglen) {
+            std::cout << "Correctly read bytes. decoding..." << std::endl;
+
+            AthenaRequest request;
+            request.ParseFromString(msg);
+            std::cout << "Parsed!" << std::endl;
+
+            if (request.has_testrequest()) {
+               const AthenaRequest_TestRequest testRequest = request.testrequest();
+               if (testRequest.has_echo()) {
+                  AthenaResponse_TestResponse testResponse;
+                  std::string echo = testRequest.echo();
+                  testResponse.set_allocated_echo(&echo);
+                  AthenaResponse response;
+                  response.set_allocated_testresponse(&testResponse);
+                  response.SerializeToString(&pb);
+                  msglen = pb.length();
+#ifndef BOOST_LITTLE_ENDIAN
+                  msglen = (msglen << 8) || (msglen >> 8);
+#endif
+                  boost::asio::write(socket_, boost::asio::buffer(lengthbuf));
+                  boost::asio::write(socket_, boost::asio::buffer(pb), error);
+                  std::cout << "Responded!" << std::endl;
+               }
+            }
+            std::cout << "Finished handling request" << std::endl;
+        } else {
+            std::cout << "Did not read enough bytes (" << error << ")" << std::endl;
+        }
+    }
+
+    std::cout << "Read failed: " << error << std::endl;
+    
    // might actually be able to use a local variable here, because "Hello,
    // World!" is static, but in general we can't, because the lifetime of the
    // variable must be as long as needed for the write to finish
