bebccbac52a17505e5c2f306a0dedbdf0827ce8d
Bryan Fink <bfink@vmware.com>
2018-02-24 12:57:50 -0800

    some protobuf style guide and naming changes
    fields are supposed to be lower_snake_case
    un-nesting messages provide better names for their classes

--- src/helen_athena_conn.erl
+++ src/helen_athena_conn.erl
@@ -10,14 +10,14 @@
 
 send_echo_request(String) ->
     Msg = #athenarequest{
-             testrequest =
-                 #athenarequest_testrequest{
+             test_request =
+                 #testrequest{
                     echo = String
                    }},
     case helen_athena_conn:send(athena_pb:encode(Msg)) of
-        {ok, #athenaresponse{testresponse=#athenaresponse_testresponse{echo=S}}} when S /= undefined ->
+        {ok, #athenaresponse{test_response=#testresponse{echo=S}}} when S /= undefined ->
             {ok, S};
-        {ok, #athenaresponse{testresponse=#athenaresponse_testresponse{}}} ->
+        {ok, #athenaresponse{test_response=#testresponse{}}} ->
             {error, <<"no echo">>};
         {ok, #athenaresponse{}} ->
             {error, <<"no testresponse">>};
@@ -35,8 +35,8 @@ send(Message, {IP, Port}) ->
             case gen_tcp:send(Socket, [<<(iolist_size(Message)):2/little-unsigned-integer-unit:8>>, Message]) of
                 ok ->
                     case gen_tcp:recv(Socket, 0, 2000) of
-                        {ok, <<_Length:2/little-unsigned-integer-unit:8, Reply/binary>>} ->
-                            case athena_pb:decode_athenaresponse(Reply) of
+                        {ok, <<Length:2/little-unsigned-integer-unit:8, Reply/binary>>} ->
+                            case athena_pb:decode_athenaresponse(<<Reply:Length/binary>>) of
                                 #athenaresponse{}=R ->
                                     {ok, R};
                                 Other ->
