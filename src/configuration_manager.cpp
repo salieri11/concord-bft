d2e910b506b3d997735a804abcaf347ef9a70024
amit barve <barvea@vmware.com>
2018-03-19 15:46:56 -0700

    support for configuration file via boost::program_options

---
+++ src/configuration_manager.cpp
@@ -0,0 +1,102 @@
+// Copyright 2018 VMware, all rights reserved
+
+#include "configuration_manager.hpp"
+
+using namespace boost::program_options;
+
+using namespace std;
+
+// default IP on which to listen for client connections
+static const string default_listen_ip = "0.0.0.0";
+
+// default port on which to listen for client connections
+static const short default_listen_port = 5458;
+
+// default location of logging properties file
+static const string default_log_props = "./resources/log4cplus.properties";
+
+// default location of configuration file
+static const string default_config_file = "./resources/athena.config";
+
+// default period to check for logging properties changes (milliseconds)
+static const int default_log_props_time_ms = 60000; // 60sec
+
+
+variables_map initialize_config(int argc, char **argv) {
+   // A map to hold key-value pairs of all options
+   variables_map options_map;
+
+   // holds the value of configuration file for Athena
+   // this is NOT same as logger configuration file. Logger
+   // configuration file can be specified by command line options or
+   // as a property in configuration file.
+   string config_file;
+
+   // Program options which are generic for most of the programs:
+   // These are not available via configuration files
+   // only allowed to be passed on command line
+   options_description generic{"Generic Options"};
+   generic.add_options()
+      ("help,h",
+       "Print this help message")
+      ("config,c",
+       value<string>(&config_file)->default_value(default_config_file),
+       "Path for configuration file")
+      ;
+
+   // The configuration parameters specific to this program
+   // These can be provided in config file as well as on command line
+   // If same parameter is provided in config file as well as on
+   // command line, then command line value will take preference.
+   options_description config{"Configuration Options"};
+   config.add_options()
+      ("ip",
+       value<std::string>()->default_value(default_listen_ip),
+       "IP on which to expose the service")
+      ("port,p",
+       value<short>()->default_value(default_listen_port),
+       "Port on which to expose the service")
+      ("logger_config",
+       value<string>()->default_value(default_log_props),
+       "Path to logging properties file")
+      ("logger_reconfig_time",
+       value<int>()->default_value(default_log_props_time_ms),
+       "Interval (in ms) to check for updates to logging properties file");
+
+   options_description all_options; // description of all options
+   all_options.add(generic).add(config);
+
+   // First we parse command line options and see if --help
+   // options was provided. In this case we don't need to
+   // go for parsing config file. Otherwise call notify
+   // for command line options and move to parsing config file
+   store(command_line_parser(argc, argv).
+         options(all_options).run(), options_map);
+
+   // If cmdline options specified --help then we don't want
+   // to do further processing for command line or
+   // config file options
+   if (options_map.count("help")) {
+      std::cout << "VMware Project Athena" << std::endl;
+      std::cout << all_options << std::endl;
+      return options_map;
+   }
+
+   // call notify after checking "help", so that required
+   // parameters are not required to get help (this call throws an
+   // exception to exit the program if any parameters are invalid)
+   notify(options_map);
+
+   // Parse config file and populate map with those parameters
+   // provided in config file.
+   ifstream ifs(config_file.c_str());
+   if (!ifs) {
+      cerr << "Can not open config file: " << config_file
+           << "\n" << " Going ahead with only command line options\n";
+   } else {
+      store(parse_config_file(ifs, config), options_map);
+      notify(options_map);
+   }
+
+   return options_map;
+}
