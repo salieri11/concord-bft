9552cfe453ed0521cfdb2e90d460c1ca2916f0f8
amit barve <barvea@vmware.com>
2018-03-30 16:17:52 -0700

    Adds unit testing support with Google Test framework. Includes json parser from `https://github.com/nlohmann/json` to enable json parsing (for genesis block).

---
+++ test/utils_test.cpp
@@ -0,0 +1,14 @@
+#include "common/utils.hpp"
+#include "gtest/gtest.h"
+
+using namespace std;
+namespace athena = com::vmware::athena;
+using json = nlohmann::json;
+
+namespace {
+TEST(utils_test, parses_genesis_block) {
+   string genesis_test_file = "resources/genesis.json";
+   json pj = athena::parse_genesis_block(genesis_test_file);
+   cout << pj;
+}
+}
