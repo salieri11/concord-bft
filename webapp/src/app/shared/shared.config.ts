38d27b4499aae0e43c1d9b66d39529f91f404300
Jeremy Grebner <jeremy.grebner@toolbox9.com>
2018-04-20 10:58:55 -0400

    Add missing copyright header.

--- webapp/src/app/shared/shared.config.ts
+++ webapp/src/app/shared/shared.config.ts
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2018 VMware, all rights reserved.
+ */
+
 import { InjectionToken } from '@angular/core';
 
 export const ATHENA_API_PREFIX = new InjectionToken<string>('AthenaApiPrefix');
