a18d6b1d0714de97bf208e61679bb3bf8c736c90
Jimesh Chokshi <jchokshi@vmware.com>
2018-04-26 10:44:25 -0700

    Modified interface definition

--- src/main/java/Servlets/IEthRPC.java
+++ src/main/java/Servlets/IEthRPC.java
@@ -1,15 +1,39 @@
 package Servlets;
 
-import com.vmware.athena.Athena;
+import com.vmware.athena.Athena.EthRequest;
+import com.vmware.athena.Athena.EthResponse;
+
+import configurations.ConfigurationFactory;
+import configurations.ConfigurationFactory.ConfigurationType;
+import configurations.IConfiguration;
+
 import org.json.simple.JSONArray;
-import com.vmware.athena.Athena.AthenaRequest;
-import com.vmware.athena.Athena.AthenaResponse;
 import org.json.simple.JSONObject;
 
 public interface IEthRPC {
 
-   Athena.EthRequest buildRequest(JSONArray params) throws Exception;
+   EthRequest buildRequest(JSONObject requestJson) throws Exception;
 
-   JSONObject buildResponse(Athena.EthResponse ethResponse, JSONArray params);
+   JSONObject buildResponse(EthResponse ethResponse, JSONObject requestJson);
 
+   static EthRequest.Builder
+          initializeRequestObject(JSONObject requestJson) throws Exception {
+      return null;
+   }
+
+   static JSONArray
+          extractRequestParams(JSONObject requestJson) throws Exception {
+      return null;
+   }
+
+   @SuppressWarnings("unchecked")
+   static JSONObject initializeResponseObject(EthResponse ethResponse) {
+      IConfiguration _conf
+         = ConfigurationFactory.getConfiguration(ConfigurationType.File);
+
+      JSONObject respObject = new JSONObject();
+      respObject.put("id", ethResponse.getId());
+      respObject.put("jsonrpc", _conf.getStringValue("JSONRPC"));
+      return respObject;
+   }
 }
