bfa9e4f458b995255a19b4f58265f67a25842551
Mason Turner <tb9-mason@Masons-MacBook-Pro.local>
2018-04-26 15:34:28 -0400

    [resolves #123] Some cleanup and additional functionality in the testing UI
    Changes tab and button wording on transaction creation tab for more clarity. Adds support for the value parameter and updates field validations dynamically based on input contents. Adds support for eth_call and dynamically updates validation based on which call type the user selects.

--- webapp/src/app/testing/testing-ground/testing-ground.component.ts
+++ webapp/src/app/testing/testing-ground/testing-ground.component.ts
@@ -7,7 +7,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import 'rxjs/add/operator/mergeMap';
 
 import { EthApiService } from '../../shared/eth-api.service';
-import { AthenaApiService } from '../../shared/athena-api.service';
 
 import { ADDRESS_LENGTH, ADDRESS_PATTERN } from '../../shared/shared.config';
 
@@ -16,6 +15,11 @@ const addressValidators = [
   Validators.minLength(ADDRESS_LENGTH),
   Validators.pattern(ADDRESS_PATTERN)
 ];
+
+enum TransactionActionOptions {
+  call = 'call',
+  transaction = 'transaction'
+}
 
 @Component({
   selector: 'app-testing-ground',
@@ -35,19 +39,54 @@ export class TestingGroundComponent implements OnInit {
 
   private dataHash: string = undefined;
   private smartContractHash: string = undefined;
+  private transactionActionOptions = TransactionActionOptions;
 
   constructor(private ethApiService: EthApiService,
-              private athenaApiService: AthenaApiService,
               private formBuilder: FormBuilder,
               private changeDetectorRef: ChangeDetectorRef) {
 
     this.dataForm = this.formBuilder.group({
-      from: ['', [Validators.required, ...addressValidators]],
-      to:   ['', [Validators.required, ...addressValidators]],
-      text: ['', [Validators.required, Validators.pattern(ADDRESS_PATTERN)]],
+      from:  ['', [Validators.required, ...addressValidators]],
+      to:    ['', [Validators.required, ...addressValidators]],
+      value: ['', Validators.required],
+      text:  ['', [Validators.required, Validators.pattern(ADDRESS_PATTERN)]],
+      type:  [TransactionActionOptions.transaction],
     });
 
     this.dataForm.valueChanges.subscribe(() => this.dataHash = undefined);
+
+    this.dataForm.controls.type.valueChanges.subscribe((changes) => {
+      switch (changes) {
+        case TransactionActionOptions.transaction:
+          this.dataForm.controls.from.setValidators([Validators.required, ...addressValidators]);
+          this.dataForm.controls.from.updateValueAndValidity({emitEvent : false});
+          break;
+        case TransactionActionOptions.call:
+          this.dataForm.controls.from.clearValidators();
+          this.dataForm.controls.from.updateValueAndValidity({emitEvent : false});
+          break;
+      }
+    });
+
+    this.dataForm.controls.text.valueChanges.subscribe((changes) => {
+      if (changes.length > 0) {
+        // Value is no longer required
+        this.dataForm.controls.value.clearValidators();
+        this.dataForm.controls.text.setValidators([Validators.required, Validators.pattern(ADDRESS_PATTERN)]);
+        this.dataForm.controls.value.updateValueAndValidity({emitEvent : false});
+        this.dataForm.controls.text.updateValueAndValidity({emitEvent : false});
+      }
+    });
+
+    this.dataForm.controls.value.valueChanges.subscribe((changes) => {
+      if (changes.length > 0) {
+        // Text is no longer required
+        this.dataForm.controls.text.clearValidators();
+        this.dataForm.controls.value.setValidators([Validators.required, Validators.pattern(ADDRESS_PATTERN)]);
+        this.dataForm.controls.text.updateValueAndValidity({emitEvent : false});
+        this.dataForm.controls.value.updateValueAndValidity({emitEvent : false});
+      }
+    });
 
     this.smartContractForm = this.formBuilder.group({
       from: ['', [Validators.required, ...addressValidators]],
@@ -78,10 +117,35 @@ export class TestingGroundComponent implements OnInit {
   }
 
   onSubmitData() {
+    switch (this.dataForm.value.type) {
+      case TransactionActionOptions.transaction:
+        this.submitTransaction();
+        break;
+      case TransactionActionOptions.call:
+        this.submitCall();
+        break;
+    }
+  }
+
+  submitTransaction() {
     this.ethApiService.sendTransaction({
       from: this.dataForm.value.from,
       to: this.dataForm.value.to,
-      data: this.dataForm.value.text,
+      data: this.dataForm.value.text.length === 0 ? null : this.dataForm.value.text,
+      value: this.dataForm.value.value.length === 0 ? null : this.dataForm.value.value,
+    }).subscribe(response => {
+      this.dataHash = response.result;
+    }, response => {
+      alert(response.error);
+    });
+  }
+
+  submitCall() {
+    this.ethApiService.sendCall({
+      from: this.dataForm.value.from.length === 0 ? null : this.dataForm.value.from,
+      to: this.dataForm.value.to,
+      data: this.dataForm.value.text.length === 0 ? null : this.dataForm.value.text,
+      value: this.dataForm.value.value.length === 0 ? null : this.dataForm.value.value,
     }).subscribe(response => {
       this.dataHash = response.result;
     }, response => {
