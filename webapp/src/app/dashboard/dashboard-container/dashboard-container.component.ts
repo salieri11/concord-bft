df1cda18ddc2407b4cb73661e056e4ff6fb39791
Mason Turner <tb9-mason@Masons-MacBook-Pro.local>
2018-05-08 17:41:13 -0400

    [resolves #136] Loads actual transactions on dashboard and node detail screens and links to them from table actions. Removes placeholder text/actions from tables.

--- webapp/src/app/dashboard/dashboard-container/dashboard-container.component.ts
+++ webapp/src/app/dashboard/dashboard-container/dashboard-container.component.ts
@@ -3,6 +3,8 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import { AthenaApiService } from '../../shared/athena-api.service';
+import { BlockListingBlock } from '../../shared/remote-interfaces';
 
 @Component({
   selector: 'app-dashboard-container',
@@ -11,6 +13,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardContainerComponent implements OnInit {
 
+  blocks: BlockListingBlock[];
+  blockTransactions: any[] = [];
+  recentTransactions: any[] = [];
   mockStats = {
     totalActiveNodes: 28458,
     inactiveNodes: 583,
@@ -19,9 +24,42 @@ export class DashboardContainerComponent implements OnInit {
     averageValidationTime: 1.98
   };
 
-  constructor() { }
+  constructor(private athenaApiService: AthenaApiService) { }
 
   ngOnInit() {
+    // Get blocks, then get individual block, then build list of recent transactions from the data returned
+    // This is temporary until there is an endpoint to fetch recent transactions
+    this.athenaApiService.getBlocks().subscribe((resp) => {
+      this.blocks = resp.blocks;
+      this.fetchAndBuildBlockTransactions();
+    });
+  }
+
+  fetchAndBuildBlockTransactions() {
+    // Temporary solution pending a recent transactions endpoint
+    if (this.blocks.length) {
+      this.athenaApiService.getBlock(this.blocks[0].number).subscribe((resp) => {
+        const transactions: any = resp.transactions;
+        transactions.map(x => x.blockNumber = this.blocks[0].number);
+        this.blockTransactions = this.blockTransactions.concat(transactions);
+        this.blocks.shift();
+        this.fetchAndBuildBlockTransactions();
+      });
+    } else {
+      this.fetchAndBuildRecentTransactions();
+    }
+  }
+
+  fetchAndBuildRecentTransactions() {
+    // Temporary solution pending a recent transactions endpoint
+    if (this.blockTransactions.length) {
+      this.athenaApiService.getTransaction(this.blockTransactions[0].hash).subscribe((resp) => {
+        const transaction = {blockNumber: this.blockTransactions[0].blockNumber, ...resp};
+        this.recentTransactions.push(transaction);
+        this.blockTransactions.shift();
+        this.fetchAndBuildRecentTransactions();
+      });
+    }
   }
 
 }
--- webapp/src/app/nodes/node-detail-container/node-detail-container.component.ts
+++ webapp/src/app/nodes/node-detail-container/node-detail-container.component.ts
@@ -5,6 +5,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { TransactionFiltersModalComponent } from '../transaction-filters-modal/transaction-filters-modal.component';
+import {BlockListingBlock} from "../../shared/remote-interfaces";
+import {AthenaApiService} from "../../shared/athena-api.service";
 
 @Component({
   selector: 'app-node-detail-container',
@@ -14,9 +16,46 @@ import { TransactionFiltersModalComponent } from '../transaction-filters-modal/t
 export class NodeDetailContainerComponent implements OnInit {
   @ViewChild('filterModal') filterModal: TransactionFiltersModalComponent;
 
-  constructor() { }
+  blocks: BlockListingBlock[];
+  blockTransactions: any[] = [];
+  recentTransactions: any[] = [];
+
+  constructor(private athenaApiService: AthenaApiService) { }
 
   ngOnInit() {
+    // Get blocks, then get individual block, then build list of recent transactions from the data returned
+    // This is temporary until there is an endpoint to fetch recent transactions
+    this.athenaApiService.getBlocks().subscribe((resp) => {
+      this.blocks = resp.blocks;
+      this.fetchAndBuildBlockTransactions();
+    });
+  }
+
+  fetchAndBuildBlockTransactions() {
+    // Temporary solution pending a recent transactions endpoint
+    if (this.blocks.length) {
+      this.athenaApiService.getBlock(this.blocks[0].number).subscribe((resp) => {
+        const transactions: any = resp.transactions;
+        transactions.map(x => x.blockNumber = this.blocks[0].number);
+        this.blockTransactions = this.blockTransactions.concat(transactions);
+        this.blocks.shift();
+        this.fetchAndBuildBlockTransactions();
+      });
+    } else {
+      this.fetchAndBuildRecentTransactions();
+    }
+  }
+
+  fetchAndBuildRecentTransactions() {
+    // Temporary solution pending a recent transactions endpoint
+    if (this.blockTransactions.length) {
+      this.athenaApiService.getTransaction(this.blockTransactions[0].hash).subscribe((resp) => {
+        const transaction = {blockNumber: this.blockTransactions[0].blockNumber, ...resp};
+        this.recentTransactions.push(transaction);
+        this.blockTransactions.shift();
+        this.fetchAndBuildRecentTransactions();
+      });
+    }
   }
 
   onOpenFilterModal() {
