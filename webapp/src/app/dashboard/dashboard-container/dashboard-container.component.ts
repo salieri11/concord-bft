6738a74989dc18871e587c8bb193236ee35bb05d
Mason Turner <tb9-mason@Masons-MacBook-Pro.local>
2018-05-11 17:47:31 -0400

    Refactors fetching recent transactions.
    Moves in to the Athena API service. Makes use of some more rxjs.

--- webapp/src/app/dashboard/dashboard-container/dashboard-container.component.ts
+++ webapp/src/app/dashboard/dashboard-container/dashboard-container.component.ts
@@ -28,39 +28,8 @@ export class DashboardContainerComponent implements OnInit {
   constructor(private athenaApiService: AthenaApiService) { }
 
   ngOnInit() {
-    // Get blocks, then get individual block, then build list of recent transactions from the data returned
-    // This is temporary until there is an endpoint to fetch recent transactions
-    this.athenaApiService.getBlocks(1000).subscribe((resp) => {
-      this.blocks = resp.blocks;
-      this.fetchAndBuildBlockTransactions();
+    this.athenaApiService.getRecentTransactions().subscribe((resp) => {
+      this.recentTransactions = resp;
     });
   }
-
-  fetchAndBuildBlockTransactions() {
-    // Temporary solution pending a recent transactions endpoint
-    if (this.blocks.length) {
-      this.athenaApiService.getBlock(this.blocks[0].number).subscribe((resp) => {
-        const transactions: any = resp.transactions;
-        transactions.map(x => x.blockNumber = this.blocks[0].number);
-        this.blockTransactions = this.blockTransactions.concat(transactions);
-        this.blocks.shift();
-        this.fetchAndBuildBlockTransactions();
-      });
-    } else {
-      this.fetchAndBuildRecentTransactions();
-    }
-  }
-
-  fetchAndBuildRecentTransactions() {
-    // Temporary solution pending a recent transactions endpoint
-    if (this.blockTransactions.length) {
-      this.athenaApiService.getTransaction(this.blockTransactions[0].hash).subscribe((resp) => {
-        const transaction = {blockNumber: this.blockTransactions[0].blockNumber, ...resp};
-        this.recentTransactions.push(transaction);
-        this.blockTransactions.shift();
-        this.fetchAndBuildRecentTransactions();
-      });
-    }
-  }
-
 }
--- webapp/src/app/shared/athena-api.service.ts
+++ webapp/src/app/shared/athena-api.service.ts
@@ -11,8 +11,11 @@ import {
   Member,
   Block,
   BlockListing,
-  Transaction
+  Transaction, BlockListingBlock
 } from './remote-interfaces';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class AthenaApiService {
@@ -39,6 +42,35 @@ export class AthenaApiService {
 
   getTransaction(transactionHash: string) {
     return this.httpClient.get<Transaction>(this.apiPath(`/transactions/${transactionHash}`));
+  }
+
+  getRecentTransactions() {
+    // Get blocks, then get individual block, then build list of recent transactions from the data returned
+    // This is temporary until there is an endpoint to fetch recent transactions
+
+    return this.getBlocks(1000).flatMap(resp => {
+      const blockObservables = resp.blocks.map((block) => this.getBlock(block.number));
+
+      return Observable.forkJoin(blockObservables).flatMap(blocksResp => {
+        let blockTransactions: any[] = [];
+
+        blocksResp.forEach((block) => {
+          const tempTransactions: any = (block as Block).transactions;
+          tempTransactions.map(x => x.blockNumber = (block as Block).number);
+          blockTransactions = blockTransactions.concat(tempTransactions);
+        });
+
+        const transactionObservables = blockTransactions.map((blockTransaction) => this.getTransaction(blockTransaction.hash));
+
+        return Observable.forkJoin(transactionObservables).flatMap(transationsResp => {
+          const transactions = transationsResp.map((transaction, index) => {
+            return { blockNumber: blockTransactions[index].blockNumber, ...transaction };
+          });
+
+          return Observable.of(transactions);
+        });
+      });
+    });
   }
 
   apiPath(path: string) {
