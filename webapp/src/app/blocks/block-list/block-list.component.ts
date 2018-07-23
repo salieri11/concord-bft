b06f717647d1508833cf6f388c2f42c3e70ed01f
MChandana <chandanamarneni1@gmail.com>
2018-07-23 10:47:23 -0400

    [HEL-53] Created reusable block-graph component that adds ngx-charts graph to block and node list components.

--- webapp/src/app/blocks/block-list/block-list.component.ts
+++ webapp/src/app/blocks/block-list/block-list.component.ts
@@ -18,11 +18,149 @@ import { BlocksService } from '../shared/blocks.service';
 export class BlockListComponent implements OnInit {
   blocks: BlockListingBlock[] = [];
   nextBlockUrl: string;
+  graphData: any[];
 
-  constructor(private blocksService: BlocksService) {}
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  dailyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": new Date("2018-12-07 10:30 AM"),
+          "value": 3,
+        },
+        {
+          "name": new Date("2018-12-07 11:30 AM"),
+          "value": 4,
+        },
+        {
+          "name": new Date("2018-12-07 01:30 PM"),
+          "value": 5,
+        },
+        {
+          "name": new Date("2018-12-07 02:30 PM"),
+          "value": 9,
+        },
+        {
+          "name": new Date("2018-12-07 07:30 PM"),
+          "value": 2,
+        }
+      ]
+    }
+  ];
+
+  weeklyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": this.days[new Date("2018-12-07 10:30 AM").getDay()],
+          "value": 12,
+        },
+        {
+          "name": this.days[new Date("2018-12-08 11:30 AM").getDay()],
+          "value": 15,
+        },
+        {
+          "name": this.days[new Date("2018-12-09 01:30 PM").getDay()],
+          "value": 17,
+        },
+        {
+          "name": this.days[new Date("2018-12-10 02:30 PM").getDay()],
+          "value": 14,
+        },
+        {
+          "name": this.days[new Date("2018-12-11 07:30 PM").getDay()],
+          "value": 18,
+        },
+        {
+          "name": this.days[new Date("2018-12-12 07:30 PM").getDay()],
+          "value": 18,
+        }
+      ]
+    }
+  ];
+
+  monthlyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": new Date("2018-12-07"),
+          "value": 25,
+        },
+        {
+          "name": new Date("2018-12-08"),
+          "value": 36,
+        },
+        {
+          "name": new Date("2018-12-09"),
+          "value": 29,
+        },
+        {
+          "name": new Date("2018-12-10"),
+          "value": 50,
+        },
+        {
+          "name": new Date("2018-12-11"),
+          "value": 28,
+        }
+      ]
+    }
+  ];
+
+  quarterlyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": new Date("2018-01-07"),
+          "value": 46,
+        },
+        {
+          "name": new Date("2018-01-17"),
+          "value": 76,
+        },
+        {
+          "name": new Date("2018-02-12"),
+          "value": 36,
+        },
+        {
+          "name": new Date("2018-03-07"),
+          "value": 66,
+        },
+        {
+          "name": new Date("2018-04-27"),
+          "value": 48,
+        }
+      ]
+    }
+  ];
+
+  constructor(private blocksService: BlocksService) {
+    //Object.assign(this, this.multi);
+    this.graphData = this.dailyData;
+  }
 
   ngOnInit() {
     this.loadInitialBlocks();
+  }
+
+  getTodaysData() {
+    this.graphData = this.dailyData;
+  }
+
+  getLastSevenDaysData() {
+    this.graphData = this.weeklyData;
+  }
+
+  getLastThirtyDaysData() {
+    this.graphData = this.monthlyData;
+  }
+
+  getLastNinetyDaysData() {
+    this.graphData = this.quarterlyData;
   }
 
   loadInitialBlocks() {
--- webapp/src/app/nodes/nodes/nodes.component.ts
+++ webapp/src/app/nodes/nodes/nodes.component.ts
@@ -24,10 +24,148 @@ export class NodesComponent implements OnInit {
 
   members: Member[] = [];
 
-  constructor(private nodesService: NodesService) {}
+  graphData: any[];
+
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  dailyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": new Date("2018-12-07 10:30 AM"),
+          "value": 3,
+        },
+        {
+          "name": new Date("2018-12-07 11:30 AM"),
+          "value": 4,
+        },
+        {
+          "name": new Date("2018-12-07 01:30 PM"),
+          "value": 5,
+        },
+        {
+          "name": new Date("2018-12-07 02:30 PM"),
+          "value": 9,
+        },
+        {
+          "name": new Date("2018-12-07 07:30 PM"),
+          "value": 2,
+        }
+      ]
+    }
+  ];
+
+  weeklyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": this.days[new Date("2018-12-07 10:30 AM").getDay()],
+          "value": 12,
+        },
+        {
+          "name": this.days[new Date("2018-12-08 11:30 AM").getDay()],
+          "value": 15,
+        },
+        {
+          "name": this.days[new Date("2018-12-09 01:30 PM").getDay()],
+          "value": 17,
+        },
+        {
+          "name": this.days[new Date("2018-12-10 02:30 PM").getDay()],
+          "value": 14,
+        },
+        {
+          "name": this.days[new Date("2018-12-11 07:30 PM").getDay()],
+          "value": 18,
+        },
+        {
+          "name": this.days[new Date("2018-12-12 07:30 PM").getDay()],
+          "value": 18,
+        }
+      ]
+    }
+  ];
+
+  monthlyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": new Date("2018-12-07"),
+          "value": 25,
+        },
+        {
+          "name": new Date("2018-12-08"),
+          "value": 36,
+        },
+        {
+          "name": new Date("2018-12-09"),
+          "value": 29,
+        },
+        {
+          "name": new Date("2018-12-10"),
+          "value": 50,
+        },
+        {
+          "name": new Date("2018-12-11"),
+          "value": 28,
+        }
+      ]
+    }
+  ];
+
+  quarterlyData = [
+    {
+      "name": "Transactions",
+      "series": [
+        {
+          "name": new Date("2018-01-07"),
+          "value": 46,
+        },
+        {
+          "name": new Date("2018-01-17"),
+          "value": 76,
+        },
+        {
+          "name": new Date("2018-02-12"),
+          "value": 36,
+        },
+        {
+          "name": new Date("2018-03-07"),
+          "value": 66,
+        },
+        {
+          "name": new Date("2018-04-27"),
+          "value": 48,
+        }
+      ]
+    }
+  ];
+
+  constructor(private nodesService: NodesService) {
+    this.graphData = this.dailyData;
+  }
 
   ngOnInit() {
     this.loadMembers();
+  }
+
+  getTodaysData() {
+    this.graphData = this.dailyData;
+  }
+
+  getLastSevenDaysData() {
+    this.graphData = this.weeklyData;
+  }
+
+  getLastThirtyDaysData() {
+    this.graphData = this.monthlyData;
+  }
+
+  getLastNinetyDaysData() {
+    this.graphData = this.quarterlyData;
   }
 
   loadMembers() {
