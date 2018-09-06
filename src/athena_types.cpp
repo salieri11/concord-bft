ee0e658daca4b1d32df2f9b29d3e519a2142b5b6
Pengfei Hu <pengfeih@vmware.com>
2018-09-06 15:44:57 -0700

    1. Fix bug for zero-value. Besides the zero-nounce, the zero-value should also be empty string, e.g.  0x80, not 0x00.
    2. Fix bug for contract address.

--- src/athena_types.cpp
+++ src/athena_types.cpp
@@ -77,12 +77,21 @@ std::vector<uint8_t> com::vmware::athena::EthTransaction::rlp() const
    rlpb.add(this->sig_r);
    rlpb.add(this->sig_v);
    rlpb.add(this->input);
-   rlpb.add(this->value);
+
+   if(this->value == 0){
+      std::vector<uint8_t> empty_value;
+      rlpb.add(empty_value);
+   }else {
+      rlpb.add(this->value);
+   }
+
    if (this->contract_address == zero_address) {
       rlpb.add(this->to);
    } else {
-      rlpb.add(this->contract_address);
+      std::vector<uint8_t> empty_address;
+      rlpb.add(empty_address);
    }
+
    rlpb.add(this->gas_limit);
    rlpb.add(this->gas_price);
    if (this->nonce == 0) {
