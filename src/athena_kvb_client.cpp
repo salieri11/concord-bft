4980947b98db8cd56b1c53dcb54459eab22b59cf
Igor Golikov <igolikov@vmware.com>
2018-09-17 10:42:26 +0300

    integration continued, most of test pass still no state transfer and no peer info

--- src/athena_kvb_client.cpp
+++ src/athena_kvb_client.cpp
@@ -34,12 +34,12 @@ bool com::vmware::athena::KVBClient::send_request_sync(AthenaRequest &req,
       cmdslice, isReadOnly, replyslice, actualReplySize);
 
    if (status.ok() && replyslice.size() > 0) {
-      return resp.ParseFromArray(replyslice.data(), replyslice.size());
+      return resp.ParseFromArray(replyslice.data(), actualReplySize);
    } else {
       LOG4CPLUS_ERROR(logger_, "Error invoking "
                       << (isReadOnly ? "read-only" : "read-write")
                       << " command. Status: " << status.ToString()
-                      << " Reply size: " << replyslice.size());
+                      << " Reply size: " << actualReplySize);
       ErrorResponse *err = resp.add_error_response();
       err->set_description("Internal Athena Error");
       return true;
