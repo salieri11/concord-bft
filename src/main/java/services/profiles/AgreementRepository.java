d5602feba304173efc5a3f821ab4cd036eec3de6
Matthew Harrison <mharrison@vmware.com>
2018-09-19 08:09:16 -0700

    Added Agreements API and Table

---
+++ src/main/java/services/profiles/AgreementRepository.java
@@ -0,0 +1,14 @@
+/**
+ * <p>
+ * Copyright 2018 VMware, all rights reserved.
+ * </p>
+ *
+ */
+
+package services.profiles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgreementRepository extends JpaRepository<Agreement, Long> {
+
+}
---
+++ src/main/java/services/profiles/AgreementsRegistryManager.java
@@ -0,0 +1,71 @@
+/**
+ * <p>
+ * Copyright 2018 VMware, all rights reserved.
+ * </p>
+ *
+ */
+
+package services.profiles;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class manages all persistence related operations related to Agreement
+ * management API.
+ */
+@Component
+@Transactional
+public class AgreementsRegistryManager {
+
+   @Autowired
+   private AgreementRepository agreementRepository;
+
+   /** Needed for spring */
+   protected AgreementsRegistryManager() {
+   }
+
+   public JSONObject getAgreementWithID(String id) {
+      JSONObject json = new JSONObject();
+      Optional<Agreement> oAgreement = agreementRepository.findById(Long.parseLong(id));
+
+      if (oAgreement.isPresent()) {
+         Agreement a = oAgreement.get();
+         Boolean accepted = a.getAcceptance();
+         json.put("id", a.getID());
+         json.put("type", a.getType());
+         json.put("accepted", a.getAcceptance());
+
+         // Don't send legal agreement if already accepted
+         // it's too heavy
+         if (accepted == Boolean.FALSE) {
+            json.put("content", a.getContent());
+         }
+
+      }
+
+      return json;
+   }
+
+   public JSONObject updateAgreement(String id, JSONObject request) {
+      JSONObject json = new JSONObject();
+      Optional<Agreement> oAgreement = agreementRepository.findById(
+                                       Long.parseLong(id));
+
+      if (oAgreement.isPresent()) {
+         Agreement agreement = oAgreement.get();
+         if (request.get("accepted") == Boolean.TRUE) {
+            agreement.accepted();
+         }
+         agreementRepository.save(agreement);
+      }
+
+      return json;
+   }
+}
