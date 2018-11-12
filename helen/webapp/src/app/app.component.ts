cefa0f2ba8d73863cfcc2a392b3ac2d454b50637
Matthew Harrison <mharrison@vmware.com>
2018-11-12 15:51:32 -0800

    Remove Athena From Page Title
    - Found more athena in the code, replacing it with vmware blockchain

--- helen/webapp/src/app/app.component.ts
+++ helen/webapp/src/app/app.component.ts
@@ -2,14 +2,31 @@
  * Copyright 2018 VMware, all rights reserved.
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'athena-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+    private titleService: Title,
+    private translateService: TranslateService,
+  ) {}
+
+  ngOnInit() {
+    this.setTitle();
+  }
+
+  private setTitle() {
+    this.titleService.setTitle(
+      this.translateService.instant('title')
+    );
+  }
+
 }
