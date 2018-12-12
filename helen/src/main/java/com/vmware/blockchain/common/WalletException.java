80e387fd5b55245f4271a8479659b53774563a40
Pengfei Hu <pengfeih@vmware.com>
2018-12-11 16:52:07 -0800

    add copyright and overview comment for walletException

--- helen/src/main/java/com/vmware/blockchain/common/WalletException.java
+++ helen/src/main/java/com/vmware/blockchain/common/WalletException.java
@@ -1,5 +1,12 @@
+/*
+ * Copyright (c) 2018 VMware, Inc. All rights reserved. VMware Confidential
+ */
+
 package com.vmware.blockchain.common;
 
+/**
+ * There was an error trying to create the wallet for user.
+ */
 public class WalletException extends Exception {
     private static final long serialVersionUID = 1L;
 
