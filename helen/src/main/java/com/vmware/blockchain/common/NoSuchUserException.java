d4d6576ffef4aa0e12718a62f05dde80ca04aa5f
Pengfei Hu <pengfeih@vmware.com>
2019-01-09 14:44:08 -0800

    add comments for meet the checkstyle requirements

--- helen/src/main/java/com/vmware/blockchain/common/NoSuchUserException.java
+++ helen/src/main/java/com/vmware/blockchain/common/NoSuchUserException.java
@@ -1,7 +1,14 @@
+/*
+ * Copyright (c) 2018 VMware, Inc. All rights reserved. VMware Confidential
+ */
+
 package com.vmware.blockchain.common;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * There was an error when the user does not exist.
+ */
 public class NoSuchUserException extends HelenException {
     private static final long serialVersionUID = 1L;
 
