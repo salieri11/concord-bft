7ad1d6f738af598fc47bf7fe39928208f7c72edb
Ramkumar SG <rsg@vmware.com>
2019-03-11 16:47:16 -0700

    Fix Asset Transfer Testruns

--- buildall.sh
+++ buildall.sh
@@ -186,7 +186,7 @@ addToProcList "Cockroach_DB" $!
 docker pull athena-docker-local.artifactory.eng.vmware.com/reverse-proxy:0.1.2 &
 addToProcList "Reverse_proxy" $!
 
-docker build asset-transfer -f asset-transfer/Dockerfile -t "${asset_transfer_repo}:${asset_transfer_tag}" --label ${version_label}=${asset_transfer_tag} --label ${commit_label}=${commit_hash} &
+docker build asset-transfer -f asset-transfer/Dockerfile -t "${asset_transfer_repo}:${asset_transfer_tag}" --label ${version_label}=${asset_transfer_tag} --label ${commit_label}=${commit_hash} > asset_transfer_build.log 2>&1 &
 addToProcList "Asset_Transfer_sample_image" $!
 
 docker build agent -f agent/packaging.Dockerfile -t ${agent_repo}:${agent_tag} --label ${version_label}=${agent_tag} --label ${commit_label}=${commit_hash} > agent_build.log 2>&1 &
