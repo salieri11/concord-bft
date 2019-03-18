65cd79841a1dd1f53900e77a8e80bf596841cc3b
kdesai50 <47374630+kdesai50@users.noreply.github.com>
2019-03-18 10:59:15 -0700

    VB-379 - For now, holding off more work on 'agent' to focus on deployment service. (#220)
    This change is for adding more unit-tests for agent.

--- agent/src/test/java/com/vmware/concord/agent/HttpRequestTest.java
+++ agent/src/test/java/com/vmware/concord/agent/HttpRequestTest.java
@@ -15,12 +15,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.web.client.RestTemplate;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 
 import com.vmware.blockchain.agent.ConcordAgent;
 
@@ -31,6 +38,26 @@ import com.vmware.blockchain.agent.ConcordAgent;
 @WebMvcTest(secure = false, controllers = AgentRestController.class)
 @ComponentScan(basePackageClasses = {HttpRequestTest.class })
 public class HttpRequestTest {
+
+    /**
+     * For getting the Protobuf response converted to REST message.
+     */
+    @Configuration
+    public static class RestClientConfiguration {
+
+        //@Bean
+        //RestTemplate restTemplate(ProtobufHttpMessageConverter msg) {
+        //    return new RestTemplate(msg);
+        //}
+
+        @Bean
+        ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+            return new ProtobufHttpMessageConverter();
+        }
+    }
+
+    //@Autowired
+    //private RestTemplate restTemplate;
 
     @Autowired
     private MockMvc mockMvc;
@@ -58,5 +85,18 @@ public class HttpRequestTest {
         when(agentController.status()).thenReturn(status);
         mockMvc.perform(get("/api/status/"))
                             .andExpect(status().isOk()).andReturn();
+    }
+
+    @Test
+    public void getRestStatusOk() throws Exception {
+
+        ConcordAgent.ConcordAgentStatus status = ConcordAgent.ConcordAgentStatus.newBuilder()
+                           .setVersion(1)
+                           .setStatus(ConcordAgent.ConcordAgentStatus.HealthStatus.GREEN)
+                           .build();
+        when(agentController.status()).thenReturn(status);
+        MvcResult result = mockMvc.perform(get("/api/status/")).andReturn();
+        String response = result.getResponse().getContentAsString();
+        System.out.println("String response: " + response);
     }
 }
