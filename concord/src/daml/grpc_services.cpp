81ca8e2c8fb54ce52fbefe4a5de2aa3a86f9cbb9
Robert Muschner <rmuschner@vmware.com>
2019-05-03 16:08:53 -0700

    Concord: Add DAML service and commands handler
    In this change I introduce the DAML code from the prototype - the parts that
    are used to build a KV service on top of a consensus algorithm. On one hand we
    have `grpc_services` which defines all gRPC endpoints for the API server and on
    the other hand there is the commands handler which gets invoked when consensus
    is reached. Most notably, all incoming requests are packed within a
    ConcordRequest/*Response and further into a DamlRequest/*Response type. The use
    of the API services and the new commands handler will be implemented with the
    next change.
    
    Note: The code is mostly copy & paste with adaptions.
    
    FYI, in here you can see how we integrate protobuf3 messages within version2.
    Converting concord.proto to v3 wasn't as straight forward hence the workaround.

---
+++ concord/src/daml/grpc_services.cpp
@@ -0,0 +1,132 @@
+// Copyright 2019 VMware, all rights reserved
+
+#include "grpc_services.hpp"
+
+#include <string>
+
+#include "consensus/kvb/BlockchainDBTypes.hpp"
+#include "consensus/kvb/BlockchainInterfaces.h"
+#include "daml/cmd_handler.hpp"
+
+using grpc::ServerContext;
+using grpc::ServerWriter;
+using namespace com::digitalasset;
+using com::digitalasset::kvbc::Command;
+using com::digitalasset::kvbc::CommandReply;
+using com::digitalasset::kvbc::CommitResponse;
+using com::digitalasset::kvbc::CommitResponse_CommitStatus;
+using com::digitalasset::kvbc::CommitResponse_CommitStatus_OK;
+using com::digitalasset::kvbc::CommittedTx;
+using com::digitalasset::kvbc::ReadTransactionRequest;
+using com::digitalasset::kvbc::ReadTransactionResponse;
+using com::vmware::concord::ConcordRequest;
+using com::vmware::concord::ConcordResponse;
+using com::vmware::concord::DamlRequest;
+using com::vmware::concord::DamlResponse;
+
+using Blockchain::BlockId;
+using Blockchain::IClient;
+using Blockchain::ILocalKeyValueStorageReadOnly;
+using Blockchain::Key;
+using Blockchain::Value;
+
+using namespace std;
+
+namespace concord {
+namespace daml {
+
+grpc::Status DataServiceImpl::GetLatestBlockId(
+    ServerContext* context, const kvbc::GetLatestBlockIdRequest* request,
+    kvbc::BlockId* reply) {
+  reply->set_block_id(ro_storage_->getLastBlock());
+  return grpc::Status::OK;
+}
+
+grpc::Status DataServiceImpl::ReadTransaction(
+    ServerContext* context, const ReadTransactionRequest* request,
+    ReadTransactionResponse* reply) {
+  LOG4CPLUS_INFO(logger, "DataService: ReadTransaction...");
+
+  BlockId readBlockId = request->block_id();
+  if (readBlockId <= 0) {
+    readBlockId = ro_storage_->getLastBlock();
+  }
+
+  for (int i = 0; i < request->keys_size(); i++) {
+    const string& keyStr = request->keys(i);
+    Key key = createSliver(keyStr);
+    Value value;
+    BlockId outBlockId;
+    Blockchain::Status status =
+        ro_storage_->get(readBlockId, key, value, outBlockId);
+    if (status.isOK()) {
+      kvbc::KeyValuePair* kv = reply->add_results();
+      kv->set_key(keyStr);
+      kv->set_value(value.data(), value.length());
+    } else {
+      LOG4CPLUS_ERROR(logger, "DataService: key '"
+                                  << keyStr << "' was not found! " << status);
+    }
+  }
+  // FIXME(JM): Return block ids of each separate get, or return max block id?
+  reply->set_block_id(readBlockId);
+  return grpc::Status::OK;
+}
+
+grpc::Status CommitServiceImpl::CommitTransaction(
+    ServerContext* context, const kvbc::CommitRequest* request,
+    kvbc::CommitResponse* reply) {
+  LOG4CPLUS_INFO(logger, "CommitService: Transactions...");
+
+  ConcordResponse resp;
+  ConcordRequest req;
+  DamlRequest* daml_request = req.mutable_daml_request();
+
+  Command cmd;
+  cmd.mutable_commit()->CopyFrom(*request);
+
+  std::string cmd_string;
+  cmd.SerializeToString(&cmd_string);
+  daml_request->set_command(cmd_string.c_str(), cmd_string.size());
+
+  if (!pool.send_request_sync(req, false /* read-only */, resp)) {
+    LOG4CPLUS_ERROR(logger, "DAML commit transaction failed");
+    return grpc::Status::CANCELLED;
+  }
+
+  assert(resp.has_daml_response());
+  assert(resp.daml_response().has_command_reply());
+
+  CommandReply cmd_reply;
+  if (!cmd_reply.ParseFromString(resp.daml_response().command_reply())) {
+    LOG4CPLUS_ERROR(logger, "Failed to parse DAML/CommandReply");
+    return grpc::Status::CANCELLED;
+  }
+  assert(cmd_reply.has_commit());
+
+  reply->CopyFrom(cmd_reply.commit());
+  return grpc::Status::OK;
+}
+
+grpc::Status EventsServiceImpl::CommittedTxs(
+    ServerContext* context, const kvbc::CommittedTxsRequest* request,
+    ServerWriter<kvbc::CommittedTx>* writer) {
+  LOG4CPLUS_INFO(logger, "EventsService: CommittedTxs...");
+
+  BlockingPersistentQueueReader<CommittedTx> reader =
+      committedTxs_.newReader(0);
+
+  while (1) {
+    CommittedTx committedTx = reader.pop();
+    LOG4CPLUS_INFO(logger, "KVBCEventsService: Sending event for blockId "
+                               << committedTx.block_id());
+
+    if (!writer->Write(committedTx)) {
+      break;
+    }
+  }
+  return grpc::Status::OK;
+}
+
+}  // namespace daml
+}  // namespace concord
