0637a57aa9781ba19625f81f85872450b9c81d6b
machenbach <machenbach@vmware.com>
2019-05-08 15:45:11 -0700

    Adding Organization Controller tests.

---
+++ helen/src/test/java/com/vmware/blockchain/services/profiles/OrganizationContollerTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2019 VMware, Inc. All rights reserved. VMware Confidential
+ */
+
+package com.vmware.blockchain.services.profiles;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.vmware.blockchain.MvcConfig;
+import com.vmware.blockchain.common.ConcordProperties;
+import com.vmware.blockchain.connections.ConcordConnectionPool;
+import com.vmware.blockchain.dao.GenericDao;
+import com.vmware.blockchain.security.AuthHelper;
+import com.vmware.blockchain.security.JwtTokenProvider;
+import com.vmware.blockchain.security.ServiceContext;
+import com.vmware.blockchain.services.blockchains.BlockchainService;
+import com.vmware.blockchain.services.tasks.TaskService;
+
+import io.grpc.ManagedChannel;
+
+/**
+ * Test Organization Controller.
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(secure = false, controllers = { OrganizationContoller.class })
+@ContextConfiguration(classes = MvcConfig.class)
+@ComponentScan(basePackageClasses = { OrganizationContoller.class })
+public class OrganizationContollerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    OrganizationService organizationService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private ProfilesService prm;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    @MockBean
+    private ConcordProperties concordProperties;
+
+    @MockBean
+    private ConcordConnectionPool connectionPool;
+
+    @MockBean
+    private KeystoreService keystoreService;
+
+    @MockBean
+    private DefaultProfiles profiles;
+
+    @MockBean
+    private BlockchainService blockchainService;
+
+    @MockBean
+    private ConsortiumService consortiumService;
+
+    @MockBean
+    AuthHelper authHelper;
+
+    @MockBean
+    GenericDao genericDao;
+
+    @MockBean
+    ServiceContext serviceContext;
+
+    @MockBean
+    TaskService taskService;
+
+    @MockBean
+    ManagedChannel channel;
+
+    @Autowired
+    DefaultProfiles defaultProfiles;
+
+    @Autowired
+    Jackson2ObjectMapperBuilder jacksonBuilder;
+
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void init() throws Exception {
+        List<Organization> orgList =
+                ImmutableList.of(new Organization("One"), new Organization("two"));
+        Mockito.when(organizationService.list()).thenReturn(orgList);
+        objectMapper = jacksonBuilder.build();
+    }
+
+    @Test
+    void listOrgs() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/organizations").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+        String body = result.getResponse().getContentAsString();
+        List<OrganizationContoller.OrgGetResponse> res = objectMapper.readValue(body, new TypeReference<>() {});
+        // As an operator, we should see both blockchains.
+        Assertions.assertEquals(2, res.size());
+    }
+
+    @Test
+    void getOrg() {
+    }
+
+    @Test
+    void createOrg() {
+    }
+
+    @Test
+    void updateOrg() {
+    }
+}
