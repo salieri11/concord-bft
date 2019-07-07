c2efe64f9116d4e42720c7bf5078f8540040b7b6
Igor Golikov <salieri11@gmail.com>
2019-07-07 14:17:21 +0300

    cherry-pick from 73a1cd51aa85f61006f873e3573a2a2f5f5b59ee

--- bftengine/tests/simpleTest/replica.cpp
+++ bftengine/tests/simpleTest/replica.cpp
@@ -57,39 +57,6 @@
 // bftEngine includes
 #include "test_comm_config.hpp"
 #include "test_parameters.hpp"
-<<<<<<< HEAD
-#include "Logging.hpp"
-
-// simpleTest includes
-#include "commonDefs.h"
-
-#define REPLICA2_RESTART_NO_VC (1)
-#define ALL_REPLICAS_RESTART_NO_VC (0)
-#define REPLICA2_RESTART_VC (0)
-#define ALL_REPLICAS_RESTART_VC (0)
-#define PRIMARY_REPLICA_RESTART (0)
-
-static_assert(REPLICA2_RESTART_NO_VC + ALL_REPLICAS_RESTART_NO_VC + REPLICA2_RESTART_VC + ALL_REPLICAS_RESTART_VC
-                  + PRIMARY_REPLICA_RESTART <= 1, "");
-
-using namespace std;
-
-concordlogger::Logger replicaLogger = concordlogger::Log::getLogger("simpletest.replica");
-bftEngine::Replica *replica = nullptr;
-ReplicaParams rp;
-
-//void signalHandler(int signum) {
-//  if(replica)
-//    replica->stop();
-//
-//  LOG_INFO(replicaLogger, "replica " << rp.replicaId << " stopped");
-//  exit(0);
-//}
-
-#define test_assert(statement, message) \
-{ if (!(statement)) { \
-LOG_FATAL(logger, "assert fail with message: " << message); assert(false);}}
-=======
 #include "simple_test_replica.hpp"
 #include "Logging.hpp"
 
@@ -97,13 +64,12 @@ LOG_FATAL(logger, "assert fail with message: " << message); assert(false);}}
 #include <log4cplus/configurator.h>
 #endif
 
-#define REPLICA2_RESTART_NO_VC (0)
+#define REPLICA2_RESTART_NO_VC (1)
 #define ALL_REPLICAS_RESTART_NO_VC (0)
-#define REPLICA2_RESTART_VC (1)
+#define REPLICA2_RESTART_VC (0)
 #define ALL_REPLICAS_RESTART_VC (0)
 #define PRIMARY_REPLICA_RESTART (0)
 static_assert(REPLICA2_RESTART_NO_VC + ALL_REPLICAS_RESTART_NO_VC + REPLICA2_RESTART_VC + ALL_REPLICAS_RESTART_VC + PRIMARY_REPLICA_RESTART <= 1, "");
->>>>>>> 189edf4... unit test partially work, first attempt
 
 using bftEngine::ICommunication;
 using bftEngine::PlainUDPCommunication;
@@ -117,13 +83,8 @@ using bftEngine::RequestsHandler;
 using namespace std;
 using namespace bftEngine;
 
-<<<<<<< HEAD
-void parse_params(int argc, char **argv) {
-  if (argc < 2) {
-=======
 void parse_params(int argc, char** argv, ReplicaParams &rp) {
   if(argc < 2) {
->>>>>>> 189edf4... unit test partially work, first attempt
     throw std::runtime_error("Unable to read replica id");
   }
 
@@ -204,124 +165,17 @@ void parse_params(int argc, char** argv, ReplicaParams &rp) {
     } catch (std::out_of_range &e) {
       printf("One of the parameters is out of range\n");
       exit(-1);
-<<<<<<< HEAD
     }
   }
 
-}
-
-// The replica state machine.
-class SimpleAppState : public RequestsHandler {
- private:
-  uint64_t client_to_index(NodeNum clientId) {
-    return clientId - numOfReplicas;
-  }
-
-  uint64_t get_last_state_value(NodeNum clientId) {
-    auto index = client_to_index(clientId);
-    return statePtr[index].lastValue;
-  }
-
-  uint64_t get_last_state_num(NodeNum clientId) {
-    auto index = client_to_index(clientId);
-    return statePtr[index].stateNum;
-  }
-
-  void set_last_state_value(NodeNum clientId, uint64_t value) {
-    auto index = client_to_index(clientId);
-    statePtr[index].lastValue = value;
-  }
-
-  void set_last_state_num(NodeNum clientId, uint64_t value) {
-    auto index = client_to_index(clientId);
-    statePtr[index].stateNum = value;
-  }
-
- public:
-
-  SimpleAppState(uint16_t numCl, uint16_t numRep) :
-      statePtr{new SimpleAppState::State[numCl]},
-      numOfClients{numCl},
-      numOfReplicas{numRep} {}
-
-  // Handler for the upcall from Concord-BFT.
-  int execute(uint16_t clientId,
-              uint64_t sequenceNum,
-              bool readOnly,
-              uint32_t requestSize,
-              const char *request,
-              uint32_t maxReplySize,
-              char *outReply,
-              uint32_t &outActualReplySize) override {
-    if (readOnly) {
-      // Our read-only request includes only a type, no argument.
-      test_assert(requestSize == sizeof(uint64_t), "requestSize =! " << sizeof(uint64_t));
-
-      // We only support the READ operation in read-only mode.
-      test_assert(*reinterpret_cast<const uint64_t *>(request) == READ_VAL_REQ,
-                  "request is NOT " << READ_VAL_REQ);
-
-      // Copy the latest register value to the reply buffer.
-      test_assert(maxReplySize >= sizeof(uint64_t), "maxReplySize < " << sizeof(uint64_t));
-      uint64_t *pRet = reinterpret_cast<uint64_t *>(outReply);
-      auto lastValue = get_last_state_value(clientId);
-      *pRet = lastValue;
-      outActualReplySize = sizeof(uint64_t);
-    } else {
-      // Our read-write request includes one eight-byte argument, in addition to
-      // the request type.
-      test_assert(requestSize == 2 * sizeof(uint64_t), "requestSize != " << 2 * sizeof(uint64_t));
-
-      // We only support the WRITE operation in read-write mode.
-      const uint64_t *pReqId = reinterpret_cast<const uint64_t *>(request);
-      test_assert(*pReqId == SET_VAL_REQ, "*preqId != " << SET_VAL_REQ);
-
-      // The value to write is the second eight bytes of the request.
-      const uint64_t *pReqVal = (pReqId + 1);
-
-      // Modify the register state.
-      set_last_state_value(clientId, *pReqVal);
-      // Count the number of times we've modified it.
-      auto stateNum = get_last_state_num(clientId);
-      set_last_state_num(clientId, stateNum + 1);
-
-      // Reply with the number of times we've modified the register.
-      test_assert(maxReplySize >= sizeof(uint64_t), "maxReplySize < " << sizeof(uint64_t));
-      uint64_t *pRet = reinterpret_cast<uint64_t *>(outReply);
-      *pRet = stateNum;
-      outActualReplySize = sizeof(uint64_t);
-
-      st->markUpdate(statePtr, sizeof(State) * numOfClients);
-=======
->>>>>>> 189edf4... unit test partially work, first attempt
-    }
-  }
-
-<<<<<<< HEAD
-  struct State {
-    // Number of modifications made.
-    uint64_t stateNum = 0;
-    // Register value.
-    uint64_t lastValue = 0;
-  };
-  State *statePtr;
-
-  uint16_t numOfClients;
-  uint16_t numOfReplicas;
-
-  concordlogger::Logger logger = concordlogger::Log::getLogger("simpletest.replica");
-
-  bftEngine::SimpleInMemoryStateTransfer::ISimpleInMemoryStateTransfer *st = nullptr;
-};
-=======
 }
 
 SimpleTestReplica *replica;
 void signalHandler( int signum ) {
   if(replica)
     replica->stop();
+  exit(0);  
 }
->>>>>>> 189edf4... unit test partially work, first attempt
 
 int main(int argc, char **argv) {
 #ifdef USE_LOG4CPP
@@ -330,16 +184,6 @@ int main(int argc, char **argv) {
   BasicConfigurator config;
   config.configure();
 #endif
-<<<<<<< HEAD
-  parse_params(argc, argv);
-
-  // allows to attach debugger
-  if (rp.debug)
-    std::this_thread::sleep_for(chrono::seconds(20));
-
-//  signal(SIGABRT, signalHandler);
-//  signal(SIGTERM, signalHandler);
-=======
   ReplicaParams rp;
   parse_params(argc, argv, rp);
 
@@ -349,7 +193,6 @@ int main(int argc, char **argv) {
 
   signal(SIGABRT, signalHandler);
   signal(SIGTERM, signalHandler);
->>>>>>> 189edf4... unit test partially work, first attempt
 
   ReplicaConfig replicaConfig;
   TestCommConfig testCommConfig(replicaLogger);
@@ -359,13 +202,11 @@ int main(int argc, char **argv) {
   replicaConfig.viewChangeTimerMillisec = rp.viewChangeTimeout;
 
 #if REPLICA2_RESTART_NO_VC || ALL_REPLICAS_RESTART_NO_VC
-  replicaConfig.autoViewChangeEnabled = false;
+  rp.viewChangeEnabled = false;
 #elif REPLICA2_RESTART_VC || ALL_REPLICAS_RESTART_VC || PRIMARY_REPLICA_RESTART
-  replicaConfig.autoViewChangeEnabled = true;
+  rp.viewChangeEnabled = true;
 #endif
 
-<<<<<<< HEAD
-=======
   PersistencyTestInfo pti;
 #if REPLICA2_RESTART_NO_VC
   pti.replica2RestartNoVC = true;
@@ -383,7 +224,6 @@ int main(int argc, char **argv) {
   pti.primaryReplicaRestart = true;
 #endif
 
->>>>>>> 189edf4... unit test partially work, first attempt
 #ifdef USE_COMM_PLAIN_TCP
   PlainTcpConfig conf = testCommConfig.GetTCPConfig(true, rp.replicaId,
                                                     rp.numOfClients,
@@ -423,11 +263,7 @@ int main(int argc, char **argv) {
   // This is the state machine that the replica will drive.
   SimpleAppState simpleAppState(rp.numOfClients, rp.numOfReplicas);
 
-<<<<<<< HEAD
-  bftEngine::SimpleInMemoryStateTransfer::ISimpleInMemoryStateTransfer *st =
-=======
   bftEngine::SimpleInMemoryStateTransfer::ISimpleInMemoryStateTransfer* st =
->>>>>>> 189edf4... unit test partially work, first attempt
       bftEngine::SimpleInMemoryStateTransfer::create(
           simpleAppState.statePtr,
           sizeof(SimpleAppState::State) * rp.numOfClients,
@@ -438,37 +274,11 @@ int main(int argc, char **argv) {
   simpleAppState.st = st;
   SimpleTestReplica *replica = new SimpleTestReplica(comm, simpleAppState,
                                                      &replicaConfig, pti, st);
+
+  SimpleTestReplica *replica = SimpleTestReplica::create_replica(pti, rp);
   replica->start();
-<<<<<<< HEAD
-
-  while (replica->isRunning()) {
-
-#if REPLICA2_RESTART_NO_VC
-    std::this_thread::sleep_for(std::chrono::seconds(6));
-    if (replicaConfig.replicaId == 2)
-      replica->restartForDebug();
-#elif REPLICA2_RESTART_VC
-    std::this_thread::sleep_for(std::chrono::seconds(60));
-    if(replicaConfig.replicaId == 2)
-      replica->restartForDebug();
-#elif ALL_REPLICAS_RESTART_NO_VC
-    std::this_thread::sleep_for(std::chrono::seconds(2 * (2 + replicaConfig.replicaId) * (2 + replicaConfig.replicaId)));
-    replica->restartForDebug();
-#elif ALL_REPLICAS_RESTART_VC
-    std::this_thread::sleep_for(std::chrono::seconds(18 * (2+replicaConfig.replicaId)*(2+replicaConfig.replicaId)));
-    replica->restartForDebug();
-#elif PRIMARY_REPLICA_RESTART
-#error not implemented
-#else
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-#endif
-
-  }
-
-=======
   // The replica is now running in its own thread. Block the main thread until
   // sigabort, sigkill or sigterm are not raised and then exit gracefully
   replica->run();
->>>>>>> 189edf4... unit test partially work, first attempt
   return 0;
 }
