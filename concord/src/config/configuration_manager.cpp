0724487d25fe30a8be8ba579ac09de9a8d54e751
Alex Upshaw <upshawa@vmware.com>
2019-07-30 17:06:57 -0700

    Fixes Concord configuration validation bug occurring when using local loopback.
    In particular, this commit fixes a bug where Concord's logic to validate that local loopback (i.e. 127.0.0.1) is used for hosts for which it is expected could trigger an exception and crash Concord during its boot-up by attempting to determine which node a configuration file belongs to before enough information has been loaded into the config to determine this.

--- concord/src/config/configuration_manager.cpp
+++ concord/src/config/configuration_manager.cpp
@@ -2844,23 +2844,30 @@ static ConcordConfiguration::ParameterStatus validatePrincipalHost(
   // ConcordConfiguration cannot be iterated.
   ConcordConfiguration nonConstConfig = config;
 
-  // Note detectLocalNode may throw an exception if it cannot determine what
-  // node this configuration belongs to, but we do not handle this in this
-  // function because we have validated that this configuration belongs to a
-  // particular node by the time detectLocalNode is called.
   if (config.hasValue<bool>("use_loopback_for_local_hosts") &&
       config.getValue<bool>("use_loopback_for_local_hosts") &&
-      (path.index == detectLocalNode(nonConstConfig)) &&
       (value != "127.0.0.1")) {
-    *failureMessage = "Invalid host address for " + path.toString() + ": " +
-                      value +
-                      "; the value 127.0.0.1 (i.e. loopback) is expected for "
-                      "this host since it is local to this node and "
-                      "use_loopback_for_local_hosts is enabled.";
-    return ConcordConfiguration::ParameterStatus::INVALID;
-  } else {
-    return ConcordConfiguration::ParameterStatus::VALID;
+    // Note that, depending on the order order in which parameters are loaded,
+    // we may not yet be able to tell which node is local in this configuration
+    // at the time this validator is called when first loading hosts into the
+    // config. To handle this case, we return INSUFFICIENT_INFORMATION if
+    // detectLocalNode throws an exception.
+    size_t local_node_index;
+    try {
+      local_node_index = detectLocalNode(nonConstConfig);
+    } catch (const ConfigurationResourceNotFoundException& e) {
+      return ConcordConfiguration::ParameterStatus::INSUFFICIENT_INFORMATION;
+    }
+    if (path.index == local_node_index) {
+      *failureMessage = "Invalid host address for " + path.toString() + ": " +
+                        value +
+                        "; the value 127.0.0.1 (i.e. loopback) is expected for "
+                        "this host since it is local to this node and "
+                        "use_loopback_for_local_hosts is enabled.";
+      return ConcordConfiguration::ParameterStatus::INVALID;
+    }
   }
+  return ConcordConfiguration::ParameterStatus::VALID;
 }
 
 // Implementation of specifyConfiguration and other utility functions that
