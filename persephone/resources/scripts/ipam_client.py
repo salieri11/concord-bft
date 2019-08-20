c055b62217e83664cc3dd47a0dc9fe789f0dff92
James Chang <jameschang@vmware.com>
2019-08-20 00:43:27 -0700

    Add reservation option to Persephone IPAM client
    This patch adds a reservation option to IPAM client script, via
    a repeatable `--reserved` command-line option to add IPs or IP ranges
    into the reserved list so that they are protected from being handed out
    as free addresses.
    
    This patch also does minor syntax corrections to
    provisioning_client.py script.

--- persephone/resources/scripts/ipam_client.py
+++ persephone/resources/scripts/ipam_client.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import collections
 import grpc
 import io
+import logging
 import persephone.core_pb2 as core
 import persephone.ip_allocation_service_pb2 as ip_allocation_service
 import persephone.ip_allocation_service_pb2_grpc as ip_allocation_service_rpc
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 def parse_arguments() -> Dict[str, Any]:
@@ -29,9 +31,10 @@ def parse_arguments() -> Dict[str, Any]:
         help="File path to trusted server certificates"
     )
     parser.add_argument(
-        "--option",
+        "--command",
         default=None,
-        help="Valid Values: test/add/remove"
+        choices=["add", "remove", "test"],
+        help="Command option"
     )
     parser.add_argument(
         "--block-name",
@@ -47,16 +50,77 @@ def parse_arguments() -> Dict[str, Any]:
         "--subnet",
         type=int,
         default=24,
-        help="Subnet prefix for the block. Default is 24"
+        help="Subnet prefix for the block (default: 24)"
+    )
+    parser.add_argument(
+        "--reserved",
+        action="append",
+        help="Reserved IP address or address range"
     )
     return vars(parser.parse_args())
+
+
+def default_block_reservation() -> bytearray:
+    """
+    Default byte-array representing an unallocated bitmap for a 256-IP-addresses block segment.
+
+    Returns:
+        a new zero-ed out byte-array of 32 bytes.
+    """
+    return bytearray([
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    ])
+
+
+def to_block_reservation(reserved: List[str], prefix: int, broadcast: int) -> Dict[int, bytes]:
+    """
+    Convert a given list of strings of IP or IP ranges, a block prefix address, and a broadcast
+    address, and create a dictionary of 256-address block segment prefix address to 32-byte byte-
+    array representing the reserved allocations.
+
+    Args:
+        reserved (List[str]): List of IP (hex) or IP ranges (hex-hex) denoting reserved addresses.
+        prefix (int): block prefix address.
+        broadcast (int): block broadcast address.
+
+    Returns:
+        a dictionary of block segment prefix to its 32-byte-wide reserved addresses byte-array.
+    """
+    log = logging.getLogger(__name__)
+
+    reserved_allocations = collections.defaultdict(default_block_reservation)
+    for str_value in reserved:
+        value = [int("0x" + x, 16) for x in str_value.split("-", maxsplit=1)]
+        ip_range = range(value[0], value[-1] + 1)
+        for ip in ip_range:
+            if prefix <= ip <= broadcast:
+                # Find the 256-address block prefix address and offset within the block.
+                ip_block_offset = ip % (1 << 8)
+                ip_block_byte_offset = ip_block_offset >> 3
+                ip_block_bit_offset = ip_block_offset % 8
+                ip_block_prefix = ip - ip_block_offset
+
+                log.info(
+                    "Reserve %x: prefix(%x), offset(%d), byte(%d/%d)",
+                    ip, ip_block_prefix, ip_block_offset, ip_block_byte_offset, ip_block_bit_offset
+                )
+
+                byte_value = reserved_allocations[ip_block_prefix][ip_block_byte_offset]
+                byte_value |= 2**ip_block_bit_offset
+                reserved_allocations[ip_block_prefix][ip_block_byte_offset] = byte_value
+
+    return {k: bytes(v) for k, v in reserved_allocations.items()}
 
 
 def create_address_block(
         stub: ip_allocation_service_rpc.IPAllocationServiceStub,
         block_name: str,
         block_prefix: str,
-        subnet: int
+        subnet: int,
+        reserved: List[str] = None
 ) -> ip_allocation_service.CreateAddressBlockResponse:
     """
     Create an address block using supplied parameters.
@@ -66,11 +130,20 @@ def create_address_block(
         block_name (str): Name of the address block to create.
         block_prefix (str): Starting id of the block
         subnet (int): Subnet size (e.g. 22, 24, 28, 30).
+        reserved (list[str]): List of addressed reserved from allocation.
 
     Returns:
         address block creation response from server.
     """
+    log = logging.getLogger(__name__)
+
     prefix = int("0x" + block_prefix, 16)
+    broadcast = prefix + (1 << (32 - subnet)) - 1
+    reserved_allocations = to_block_reservation(reserved, prefix, broadcast)
+    for key, value in reserved_allocations.items():
+        for i in enumerate(value):
+            log.info("Reserved Block %x: %s", key, i)
+
     create_address_block_request = ip_allocation_service.CreateAddressBlockRequest(
         header=core.MessageHeader(),
         block_id=block_name,
@@ -78,17 +151,11 @@ def create_address_block(
             prefix=prefix,
             subnet=subnet
         ),
-        reserved_allocations={
-            prefix: bytes([
-                0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80
-            ])
-        }
+        reserved_allocations=reserved_allocations
     )
     create_address_block_response = stub.CreateAddressBlock(create_address_block_request)
-    print("CreateAddressBlock(): ", create_address_block_response)
+    log.info("CreateAddressBlock(): %s", create_address_block_response)
+
     return create_address_block_response
 
 
@@ -106,13 +173,15 @@ def delete_address_block(
     Returns:
         address block deletion response from server.
     """
+    log = logging.getLogger(__name__)
+
     block_name = "blocks/" + block_name
     delete_address_block_request = ip_allocation_service.DeleteAddressBlockRequest(
         header=core.MessageHeader(),
         name=block_name
     )
     delete_address_block_response = stub.DeleteAddressBlock(delete_address_block_request)
-    print("DeleteAddressBlock(): ", delete_address_block_response)
+    log.info("DeleteAddressBlock(): %s", delete_address_block_response)
 
     return delete_address_block_response
 
@@ -135,6 +204,8 @@ def smoke_test(
     Returns:
         None
     """
+    log = logging.getLogger(__name__)
+
     create_address_block_response = create_address_block(
         stub,
         block_name,
@@ -148,9 +219,9 @@ def smoke_test(
                 parent=create_address_block_response.name,
             )
             allocate_response = stub.AllocateAddress(allocate_request)
-            print("AllocateAddress(): ", allocate_response)
+            log.info("AllocateAddress(): %s", allocate_response)
         except Exception as error:
-            print(error)
+            log.error(error)
     delete_address_block(stub, block_name)
     return
 
@@ -167,10 +238,18 @@ def main():
             --block-name temp
             --block-prefix 0a0b0c00
             [--subnet 24]
+            [--reserved 0a0b0c00-0a0b0c01]
+            [--reserved 0a0b0cff]
 
     Returns:
         None
     """
+    # Setup logging.
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s",
+    )
+
     args = parse_arguments()
     if args["trusted_certs"]:
         with io.open(args["trusted_certs"], "rb") as f:
@@ -185,14 +264,13 @@ def main():
     block_prefix = args["block_prefix"]
     subnet_range = args["subnet"]
 
-    if args["option"] == "test":
+    command = args["command"]
+    if command == "test":
         smoke_test(stub, block_name, block_prefix, subnet_range)
-    elif args["option"] == "add":
-        create_address_block(stub, block_name, block_prefix, subnet_range)
-    elif args["option"] == "remove":
+    elif command == "add":
+        create_address_block(stub, block_name, block_prefix, subnet_range, args["reserved"])
+    elif command == "remove":
         delete_address_block(stub, block_name)
-    else:
-        print("unsupported args...")
 
 
 if __name__ == "__main__":
