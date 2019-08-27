355a5bebae995a74aa1f81c9fc58830896a439d8
Abhijit Mande <mandeab@vmware.com>
2019-08-27 10:41:06 -0700

    Restarting SampleDAppTests

--- vars/gitlabBuildSteps.groovy
+++ vars/gitlabBuildSteps.groovy
@@ -470,8 +470,7 @@ EOF
                         # Make sure the test framework itself can run a basic test suite.
                         echo "${PASSWORD}" | sudo -S "${python}" main.py SampleSuite --resultsDir "${sample_suite_test_logs}"
 
-                        # RV, July 12, 2019: Supply chain dapp build fails.  Filed https://jira.eng.vmware.com/browse/VB-1214
-                        # echo "${PASSWORD}" | sudo -S "${python}" main.py SampleDAppTests --dockerComposeFile ../docker/docker-compose.yml --resultsDir "${sample_dapp_test_logs}" --runConcordConfigurationGeneration
+                        echo "${PASSWORD}" | sudo -S "${python}" main.py SampleDAppTests --dockerComposeFile ../docker/docker-compose.yml --resultsDir "${sample_dapp_test_logs}" --runConcordConfigurationGeneration
 
                         echo "${PASSWORD}" | sudo -S "${python}" main.py CoreVMTests --dockerComposeFile ../docker/docker-compose.yml --resultsDir "${core_vm_test_logs}" --runConcordConfigurationGeneration
                         echo "${PASSWORD}" | sudo -S "${python}" main.py HelenAPITests --dockerComposeFile ../docker/docker-compose.yml --resultsDir "${helen_api_test_logs}" --runConcordConfigurationGeneration --logLevel debug
