ce446ad4a1cc68d78f36b1579e9355515fb383da
anamikapandey <panamika@vmware.com>
2019-10-31 15:33:54 -0700

    feature service basic

---
+++ vmbc-base/src/main/java/FeatureController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+public class FeatureController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
