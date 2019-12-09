a2babbfe905e273d45d9870d509a0a9162353072
Ramkumar SG <rsg@vmware.com>
2019-12-09 13:16:15 -0800

    remove deployment on UNSPECIFIED site

--- hermes/suites/persephone_tests.py
+++ hermes/suites/persephone_tests.py
@@ -103,8 +103,8 @@ class PersephoneTests(test_suite.TestSuite):
                                                      testLogDir)
          self.writeResult(testName, result, info)
 
-      # IPAM test
-      fileRoot = os.path.join(self._testLogDir, "IPAM_verification")
+      # Deploy a blockchain and verify IPAM
+      fileRoot = os.path.join(self._testLogDir, "ethereum_deployment")
       os.makedirs(fileRoot, exist_ok=True)
       self.args.fileRoot = fileRoot
       self.deploy_and_verify_ipam_on_4_node_fixed_site()
@@ -283,8 +283,6 @@ class PersephoneTests(test_suite.TestSuite):
             ("list_models", self._test_list_models),
             ("7_Node_DAML_Blockchain_ON-PREM",
              self._test_create_daml_blockchain_7_node_onprem),
-            ("4_Node_Blockchain_UNSPECIFIED_Site",
-             self._test_create_blockchain_4_node_unspecified_site),
             ("4_Node_Blockchain_FIXED_Site",
              self._test_create_blockchain_4_node_fixed_site),
             ("7_Node_Blockchain_FIXED_Site",
@@ -465,7 +463,7 @@ class PersephoneTests(test_suite.TestSuite):
 
       self.parse_test_status(status, msg,
                              deployment_session_id=response_deployment_session_id)
-      self.writeResult("IPAM_Verification", status, msg)
+      self.writeResult("ethereum_deployment", status, msg)
       return
 
    def mark_node_as_logged_in(self, concord_ip, concord_username,
@@ -789,41 +787,6 @@ class PersephoneTests(test_suite.TestSuite):
       return self.parse_test_status(False,
               "ListModels does not contain the added metadata: {}".format(
                  self.response_add_model_id))
-
-   def _test_create_blockchain_4_node_unspecified_site(self, cluster_size=4):
-      '''
-      Test to create a blockchain cluster with 4 nodes on UNSPECIFIED sites
-      :param cluster_size: No. of concord nodes on the cluster
-      '''
-
-      start_time = time.time()
-      log.info("Deployment Start Time: {}".format(start_time))
-      response = self.rpc_test_helper.rpc_create_cluster(
-         cluster_size=cluster_size,
-         placement_type=self.rpc_test_helper.PLACEMENT_TYPE_UNSPECIFIED)
-      if response:
-         response_session_id_json = helper.protobuf_message_to_json(response[0])
-         if "low" in response_session_id_json:
-            response_deployment_session_id = response[0]
-
-            events = self.rpc_test_helper.rpc_stream_cluster_deployment_session_events(
-               response_deployment_session_id)
-            end_time = time.time()
-            log.info("Deployment End Time: {}".format(end_time))
-            log.info("**** Time taken for this deployment: {} mins".format(
-               (end_time - start_time) / 60))
-
-            if events:
-               status, msg = self.perform_post_deployment_validations(events,
-                                                                      cluster_size,
-                                                                      response_deployment_session_id)
-               return self.parse_test_status(status, msg,
-                                             deployment_session_id=response_deployment_session_id)
-            return self.parse_test_status(False,
-                                          "Failed to fetch Deployment Events",
-                                          deployment_session_id=response_deployment_session_id)
-
-      return self.parse_test_status(False, "Failed to get a valid deployment session ID")
 
 
    def _test_create_blockchain_4_node_fixed_site(self, cluster_size=4):
