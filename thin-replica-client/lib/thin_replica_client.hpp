c9605f9ad08ab85ba4db3706789b48f78ed4fb81
Alex Upshaw <upshawa@vmware.com>
2019-12-19 17:18:39 -0800

    Implement basic ReadState functionality in the Thin Replica Client.
    This commit adds some basic implementation to ThinReplicaClient::Subscribe in the Thin Replica Client Library to allow subscribe to stream in the initial state from a Thin Replica Server. This commit also introduces extremely basic logic to collect hashes from F servers other than the one sending the state such that the state will not be returned unless at least F + 1 servers are found to agree on the state (however, this extremely basic logic gives up quickly if it cannot find agreement easily and does not include remotely robust error handling for cases involving incomplete connectivity to the servers and/or disagreement among the servers). Additional notable changes included in this commit:
    * Implementation of the RingBufferUpdateQueue that provides an UpdateQueue implementation for transferring updates between the ThinReplicaClient object and application code.
    * Minor changes to the UpdateQueue interface in light of concerns realized during the implementation of RingBufferUpdateQueue.
    * Changes to the Thin Replica Client example application to ensure it meaningfully uses the ReadState functionality added to ThinReplicaClient::Subscribe.
    * Changes to thin_replica_client/README.md to document the new expected behavior of the example application.

--- thin-replica-client/lib/thin_replica_client.hpp
+++ thin-replica-client/lib/thin_replica_client.hpp
@@ -7,7 +7,7 @@
 // file:
 //  - class UpdateQueue - A synchronized queue type to be used in transferring
 //    updates between the Thin Replica Client and an application.
-//  - class RingBufferUpdateQueue - Basic implementation of UpdateQueue
+//  - class BasicUpdateQueue - Basic implementation of UpdateQueue
 //    provided by this library.
 //  - class ThinReplicaClient - Primary interface and implementation of the thin
 //    replica client.
@@ -51,7 +51,7 @@ struct Update {
   // applications receiving updates persist at least the Block ID of the most
   // recently received update (though only after any data from the update to be
   // persisted has been persisted), as Block IDs can be used to resume a
-  // subscription without having to stream updates preceding this Block ID.
+  // subscription without having to stream updates preceding that Block ID.
   uint64_t block_id;
 
   // Actual data for this update, structured as updates to key value pairs.
@@ -60,16 +60,39 @@ struct Update {
 
 // Interface for a synchronized queue to be used to transfer updates between a
 // ThinReplicaClient and an application. The Thin Replica Client Library
-// provides RingBufferUpdateQueue as an implementation of this interface,
-// though applications are also free to provide their own implementation of
-// UpdateQueue if they have reason to do so (for example, an application could
-// provide its own implementation of an UpdateQueue if it wanted to handle how
-// memory is allocated for the queue differently). An implementation of
-// UpdateQueue should guarantee that Clear, Push, Pop, and TryPop are thread
-// safe.
+// provides BasicUpdateQueue as an implementation of this interface, though
+// applications are also free to provide their own implementation of UpdateQueue
+// if they have reason to do so (for example, an application could provide its
+// own implementation of an UpdateQueue if it wanted to handle how memory is
+// allocated for the queue differently). An implementation of UpdateQueue should
+// guarantee that ReleaseConsumers, Clear, Push, Pop, and TryPop are all
+// mutually thread safe.
 class UpdateQueue {
  public:
+  // Destructor for UpdateQueue (which UpdateQueue implementations should
+  // override). Note the UpdateQueue interface does NOT require that
+  // implementations guarantee the desctructor be thread safe the UpdateQueue
+  // functions; behavior may be undefined if any other function of an
+  // UpdateQueue executes at all concurrently with that queue's destructor after
+  // that destructor has begun executing, or at any point after the destructor
+  // has begun running (including after the destructor has completed).
+  // Furthermore, behavior is undefined if any thread is still waiting on the
+  // blocking UpdateQueue:Pop call when the destructor begins running. Code
+  // owning UpdateQueue instances should guarantee that there are no outstanding
+  // calls to the UpdateQueue's functions and that no thread will start new ones
+  // before destroying an instance. Note the ReleaseConsumers function can be
+  // used to have the queue unblock and release any threads still waiting on
+  // UpdateQueue::Pop calls.
   virtual ~UpdateQueue() {}
+
+  // Release any threads currently waiting on blocking UpdateQueue::Pop calls
+  // made to this UpdateQueue, making those calls return pointers to null;
+  // making this call also puts the UpdateQueue into a state where any new calls
+  // to the UpdateQueue::Pop function will return a pointer to null instead of
+  // waiting on new updates to become available. This function may block the
+  // caller to obtain locks if that is necessary for this operation under the
+  // UpdateQueue's implementation.
+  virtual void ReleaseConsumers() = 0;
 
   // Synchronously clear all current updates in the queue. May block the calling
   // thread as necessary to obtain any lock(s) needed for this operation.
@@ -85,16 +108,20 @@ class UpdateQueue {
   // other means.
   virtual void Push(std::unique_ptr<Update> update) = 0;
 
-  // Synchronously pop and return the update at the front of the queue. If there
-  // are no updates available in the queue, this function should block the
-  // calling thread and wait until an update that can be popped is available.
-  // This function may also block the calling thread for purposes of obtaining
-  // any lock(s) needed for these operations. This function gives ownership of
-  // an allocated Update to the calller; implementations of UpdateQueue may
-  // choose whether they give ownership of an allocated Update they own to the
-  // caller or whether they dynamically allocate memory (via
+  // Synchronously pop and return the update at the front of the queue.
+  // Normally, if there are no updates available in the queue, this function
+  // should block the calling thread and wait until an update that can be popped
+  // is available. This function may also block the calling thread for purposes
+  // of obtaining any lock(s) needed for these operations. This function gives
+  // ownership of an allocated Update to the calller; implementations of
+  // UpdateQueue may choose whether they give ownership of an allocated Update
+  // they own to the caller or whether they dynamically allocate memory (via
   // malloc/new/std::allocator(s)) to construct the Update object from data the
-  // Queue has stored by some other means.
+  // Queue has stored by some other means. If ReleaseConsumers is called, any
+  // currently waiting Pop calls will be unblocked, and will return a unique_ptr
+  // to null rather than continuing to wait for new updates. Furthermore, a call
+  // to ReleaseConsumers will cause any subsequent calls to Pop to return
+  // nullptr and will prevent them from blocking their caller.
   virtual std::unique_ptr<Update> Pop() = 0;
 
   // Synchronously pop an update from the front of the queue if one is
@@ -106,23 +133,36 @@ class UpdateQueue {
   virtual std::unique_ptr<Update> TryPop() = 0;
 };
 
-// Basic UpdateQueue implementation that stores keeps unique_ptrs to updates
-// allocated by the Push caller in a ring buffer of a size set at queue
-// construction time.
-class RingBufferUpdateQueue : public UpdateQueue {
+// Basic UpdateQueue implementation provided by this library. This class can be
+// expected to adhere to the UpdateQueue interface, but details of this class's
+// implementation should be considered subject to change as we may revise it as
+// we implement, harden, and test the Thin Replica Client Library and develop a
+// more complete understanding of the needs of this library.
+class BasicUpdateQueue : public UpdateQueue {
  private:
+  std::list<std::unique_ptr<Update>> queue_data_;
+  std::mutex mutex_;
+  std::condition_variable condition_;
+  bool release_consumers_;
+
  public:
-  // Construct a RingBufferUpdateQueue with a RingBuffer for unique_ptrs of the
-  // given size.
-  RingBufferUpdateQueue(size_t buffer_size);
+  // Construct a BasicUpdateQueue.
+  BasicUpdateQueue();
 
-  virtual ~RingBufferUpdateQueue() override;
-  RingBufferUpdateQueue(const RingBufferUpdateQueue& other);
-  RingBufferUpdateQueue(const RingBufferUpdateQueue&& other);
-  RingBufferUpdateQueue& operator=(const RingBufferUpdateQueue& other);
-  RingBufferUpdateQueue& operator=(const RingBufferUpdateQueue&& other);
+  // Copying or moving a BasicUpdateQueue is explicitly disallowed, as we do not
+  // know of a compelling use case requiring copying or moving
+  // BasicUpdateQueues, we believe semantics for these operations are likely to
+  // be messy in some caess, and we believe implementation may be non-trivial.
+  // We may revisit the decision to disallow these operations should compelling
+  // use cases for them be found in the future.
+  BasicUpdateQueue(const BasicUpdateQueue& other) = delete;
+  BasicUpdateQueue(const BasicUpdateQueue&& other) = delete;
+  BasicUpdateQueue& operator=(const BasicUpdateQueue& other) = delete;
+  BasicUpdateQueue& operator=(const BasicUpdateQueue&& other) = delete;
 
-  // Implementation of UpdateQueue interface.
+  // Implementation of UpdateQueue interface
+  virtual ~BasicUpdateQueue() override;
+  virtual void ReleaseConsumers() override;
   virtual void Clear() override;
   virtual void Push(std::unique_ptr<Update> update) override;
   virtual std::unique_ptr<Update> Pop() override;
@@ -144,6 +184,18 @@ class ThinReplicaClient final {
   std::vector<
       std::unique_ptr<com::vmware::concord::thin_replica::ThinReplica::Stub>>
       server_stubs_;
+  std::shared_ptr<UpdateQueue> update_queue_;
+  uint16_t max_faulty_;
+
+  // Function(s) for computing hashes as we anticipate they exist according to
+  // non-faulty Thin Replica Servers.
+  // XXX: We anticipate it is very possible the data type for hashes and hash
+  //      function implementation(s) are likely to change between the time of
+  //      this comment's writing and the time the Thin Replica mechanism is
+  //      hardened for production.
+  typedef size_t StateHashType;
+  StateHashType AppendToReadStateHash(
+      StateHashType, const std::pair<std::string, std::string>& kvp) const;
 
  public:
   // Constructor for ThinReplicaClient. Note that, as the ThinReplicaClient
@@ -153,7 +205,12 @@ class ThinReplicaClient final {
   // constructor is called when there already exists a ThinReplicaClient in the
   // calling process that has been constructed but not destructed. Parameters:
   // - update_queue: shared pointer to an UpdateQueue object to be used to
-  //   transfer updates from this ThinReplicaClient to the application.
+  //   transfer updates from this ThinReplicaClient to the application. Note
+  //   ThinRepliaClient guarantees it will only use the Clear and Push functions
+  //   of this queue; the ThinReplicaClient will never call Pop, TryPop,
+  //   ReleaseConsumers, or ReEnableConsumers. Furthermore, a ThinReplicaClient
+  //   guarantees it will never execute the Clear or Push functions of the queue
+  //   after that ThinReplicaClient's destructor has returned.
   // - max_faulty: Maximum number of simultaneously Byzantine-faulty servers
   //   that must be tolerated (this is equivalent to the F value for the Concord
   //   cluster the servers are from).
@@ -174,7 +231,9 @@ class ThinReplicaClient final {
                     Iterator begin_servers, Iterator end_servers)
       : logger_(
             log4cplus::Logger::getInstance("com.vmware.thin_replica_client")),
-        server_stubs_() {
+        server_stubs_(),
+        update_queue_(update_queue),
+        max_faulty_(max_faulty) {
     while (begin_servers != end_servers) {
       const auto& server_info = *begin_servers;
       server_stubs_.push_back(
@@ -183,14 +242,15 @@ class ThinReplicaClient final {
       ++begin_servers;
     }
 
-    if (server_stubs_.size() < (3 * (size_t)max_faulty + 1)) {
+    if (server_stubs_.size() < (3 * (size_t)max_faulty_ + 1)) {
       size_t num_servers = server_stubs_.size();
       server_stubs_.clear();
+      update_queue_.reset();
       throw std::invalid_argument(
           "Too few servers (" + std::to_string(num_servers) +
           ") given to ThinReplicaClient constructor to tolerate requested "
           "maximum faulty servers (" +
-          std::to_string(max_faulty) +
+          std::to_string(max_faulty_) +
           "). The number of servers must be at least (3 * max_faulty + 1).");
     }
 
@@ -199,6 +259,9 @@ class ThinReplicaClient final {
     //              ThinReplicaClient is allowed to exist at a time.
   }
 
+  // Destructor for ThinReplicaClient. Calling this destructor may block the
+  // calling thread as necessary to stop and join worker thread(s) owned by the
+  // ThinReplicaClient if the ThinReplicaClient has an active subscription.
   ~ThinReplicaClient();
 
   // Copying or moving of a ThinReplicaClient object is explicitly disallowed as
@@ -220,7 +283,9 @@ class ThinReplicaClient final {
   // supports the registration of a single condition variable for subscription
   // failure; calling this function when a condition variable is already
   // registered will overwrite the existing condition (without notifying any
-  // threads waiting on it).
+  // threads waiting on it). Also note that the ThinReplicaClient::Unsubscribe
+  // and ThinReplicaClient's destructor will not cause the ThinReplicaClient to
+  // notify a registered condition.
   void RegisterSubscriptionFailureCondition(
       std::shared_ptr<std::mutex> failure_mutex,
       std::shared_ptr<std::condition_variable> failure_condition);
@@ -230,9 +295,9 @@ class ThinReplicaClient final {
   // before sending them. If a value for last_known_block_id is given, the
   // ThinReplicaClient will begin the subscription at the point specifed by that
   // Block ID, otherwise, subscription will begin by attempting to read all
-  // current state. Once the subscription is successfully setup, each
-  // update received will be pushed to the update_queue this ThinReplicaClient
-  // was constructed with. If this ThinReplicaClient already has an active
+  // current state. Once the subscription is successfully setup, each update
+  // received will be pushed to the update_queue this ThinReplicaClient was
+  // constructed with. If this ThinReplicaClient already has an active
   // subscription open, that subscription will be ended when Subscribe is
   // called. If there are any updates leftover in update_queue when Subscribe is
   // called, the queue will be cleared. Any initial state received when opening
@@ -247,8 +312,10 @@ class ThinReplicaClient final {
 
   // End any currently open subscription this ThinReplicaClient has. Note this
   // function will not automatically clear any updates still in the
-  // update_queue. Note Unsubscribe will effectively do nothing if called for a
-  // ThinReplicaClient that has no active subscription.
+  // update_queue. Unsubscribe may block the thread calling it as necessary to
+  // stop and join worker thread(s) created by the ThinReplicaClient for the
+  // subscription being terminated. Note Unsubscribe will effectively do nothing
+  // if called for a ThinReplicaClient that has no active subscription.
   void Unsubscribe();
 
   // Acknowledge receipt of the update for a given Block ID to the thin replica
