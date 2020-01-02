7d5dcba16f894c6e58bb22691f19749c33c3079e
Robert Muschner <rmuschner@vmware.com>
2020-01-02 12:11:47 -0800

    ThinReplica: Remove macro and lower log level
    This is a follow-up to the subscription MR. We decided to avoid macros and
    hence this change replaces the macro with an inline function as suggested by
    Google's C++ style guide.

--- concord/src/storage/kvb_app_filter.cpp
+++ concord/src/storage/kvb_app_filter.cpp
@@ -84,8 +84,8 @@ void KvbAppFilter::ReadBlockRange(BlockId block_id_start, BlockId block_id_end,
 
   SetOfKeyValuePairs kvb_kvs;
 
-  LOG4CPLUS_INFO(logger_,
-                 "ReadBlockRange block " << block_id << " to " << block_id_end);
+  LOG4CPLUS_DEBUG(
+      logger_, "ReadBlockRange block " << block_id << " to " << block_id_end);
 
   for (; block_id <= block_id_end; ++block_id) {
     Status status = rostorage_->getBlockData(block_id, kvb_kvs);
@@ -123,8 +123,8 @@ KvbStateHash KvbAppFilter::ReadBlockRangeHash(BlockId block_id_start,
 
   SetOfKeyValuePairs kvb_kvs;
 
-  LOG4CPLUS_INFO(logger_, "ReadBlockRangeHash block " << block_id << " to "
-                                                      << block_id_end);
+  LOG4CPLUS_DEBUG(logger_, "ReadBlockRangeHash block " << block_id << " to "
+                                                       << block_id_end);
 
   size_t hash_out = 0;
   for (; block_id <= block_id_end; ++block_id) {
