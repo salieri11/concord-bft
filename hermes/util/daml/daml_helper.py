5ff038a80b875dfea2e3050f36f347017f7eec74
Ramkumar SG <rsg@vmware.com>
2020-03-07 07:47:53 -0800

    Ignore ContractKeysSubmitterIsMaintainerIT

--- hermes/util/daml/daml_helper.py
+++ hermes/util/daml/daml_helper.py
@@ -14,7 +14,13 @@ log = logging.getLogger(__name__)
 TEST_TOOL_NAME = "ledger-api-test-tool-100.13.52.jar"
 TEST_DARS = ["SemanticTests.dar", "Test-dev.dar", "Test-stable.dar"]
 TEST_TOOL_CONTAINER = "docker_daml_test_tool_1"
-DAML_TESTS_IGNORE_LIST=["TimeIT","LotsOfPartiesIT", "TransactionScaleIT", "TransactionServiceIT"]
+DAML_TESTS_IGNORE_LIST=[
+   "TimeIT",
+   "LotsOfPartiesIT",
+   "TransactionScaleIT",
+   "TransactionServiceIT",
+   "ContractKeysSubmitterIsMaintainerIT"
+]
 error_msg = None
 
 def upload_test_tool_dars(host='localhost', port='6861'):
