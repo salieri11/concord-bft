9a208cb257cd3e3efc5f585cd11b99368a7761c7
Dmitry Danilov <ddanilov@vmware.com>
2020-03-18 03:30:22 -0700

    BC-1056: Read trace context from DAML Ledger API
    This commit provides functionality allowing to receive a span context from DAML Ledger API(Write Service).
    After extracting the context it creates a child span from the received.
    In case the incoming span is empty, a new span is created.

--- concord/src/daml/grpc_services.cpp
+++ concord/src/daml/grpc_services.cpp
@@ -3,7 +3,9 @@
 #include "grpc_services.hpp"
 
 #include <opentracing/tracer.h>
+#include <sstream>
 #include <string>
+#include <strstream>
 #include "utils/concord_logging.hpp"
 
 #include "concord_storage.pb.h"
@@ -23,6 +25,27 @@ using grpc::ServerContext;
 
 using namespace std;
 
+static std::unique_ptr<opentracing::Span> CreateSpan(
+    const CommitRequest* request, log4cplus::Logger logger) {
+  if (!request->span_context().empty()) {
+    std::istringstream context_stream{request->span_context()};
+    auto parent_span_context =
+        opentracing::Tracer::Global()->Extract(context_stream);
+    if (parent_span_context.has_value()) {
+      return opentracing::Tracer::Global()->StartSpan(
+          "commit_transaction",
+          {opentracing::ChildOf(parent_span_context->get())});
+    }
+    LOG4CPLUS_DEBUG(logger, "CreateSpan: Failed to initialize parent span from"
+                                << request->span_context());
+  }
+  LOG4CPLUS_DEBUG(logger,
+                  "CreateSpan: No parent span provided, initialize a new one");
+  return opentracing::Tracer::Global()->StartSpan(
+      "commit_transaction",
+      {opentracing::SetTag{"cid", request->correlation_id()}});
+}
+
 namespace concord {
 namespace daml {
 
@@ -40,7 +63,8 @@ grpc::Status CommitServiceImpl::CommitTransaction(ServerContext* context,
   cmd.mutable_commit()->CopyFrom(*request);
 
   concord::utils::RAIIMDC mdc("cid", request->correlation_id());
-  auto span = opentracing::Tracer::Global()->StartSpan("commit_transaction");
+
+  auto span = CreateSpan(request, logger_);
 
   std::string cmd_string;
   cmd.SerializeToString(&cmd_string);
