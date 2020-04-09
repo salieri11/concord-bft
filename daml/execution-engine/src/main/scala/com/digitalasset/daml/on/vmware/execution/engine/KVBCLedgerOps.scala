e60f3dfc53d211ddad3f4fc2deb31a0a4e9bef6c
Jussi Maki <jussi.maki@digitalasset.com>
2020-04-09 12:40:43 +0200

    Integrate DAML batch validator
    - Batching is disabled by default and can be enabled by setting
      FEATURE_daml_pipelined_commits in concord config to true, and setting ENABLE_BATCHING
      to true in daml-ledger-api startup.
    
    - Replaces the old validation code in the pipelined validator service API, with the batch
      validator.
    
    - Bumps to newer SDK and integration-kit releases.

---
+++ daml/execution-engine/src/main/scala/com/digitalasset/daml/on/vmware/execution/engine/KVBCLedgerOps.scala
@@ -0,0 +1,85 @@
+package com.digitalasset.daml.on.vmware.execution.engine
+
+import com.daml.ledger.validator.batch.BatchLedgerOpsWithAccessControl
+import com.daml.ledger.validator.batch.BatchLedgerOpsWithAccessControl.{Key, Value}
+import com.daml.ledger.validator.privacy.{AccessControlList, PublicAccess, RestrictedAccess}
+import com.digitalasset.kvbc.daml_validator.{EventToValidator, KeyValuePair, ProtectedKeyValuePair, EventFromValidator}
+import com.digitalasset.kvbc.daml_validator.EventFromValidator.{Read, Write}
+import com.google.protobuf.ByteString
+import org.slf4j.LoggerFactory
+
+import scala.collection.mutable
+import scala.concurrent.{ExecutionContext, Future, Promise}
+
+class KVBCLedgerOps(sendEvent: EventFromValidator => Unit)(implicit val executionContext: ExecutionContext) extends BatchLedgerOpsWithAccessControl {
+  private var nextReadTag = 0
+  private val readPromises = mutable.Map.empty[Int, Promise[Seq[KeyValuePair]]]
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  def handleReadResult(result: EventToValidator.ReadResult): Unit = {
+    val readTag = result.tag.toInt
+    logger.trace(s"Handling read result for $readTag")
+
+    val promise =
+      this.synchronized {
+        readPromises.remove(readTag)
+      }.getOrElse(
+        sys.error(s"No read request for id $readTag!")
+      )
+
+    logger.trace(s"Completing promise with ${result.keyValuePairs.size} key-value pairs")
+    promise.success(result.keyValuePairs)
+  }
+
+  override def read(keys: Seq[Key]): Future[Seq[Option[Value]]] = {
+    // Create a promise that we'll fulfill from [[handleReadResult]] when the
+    // result arrives.
+    val promise = Promise[Seq[(KeyValuePair)]]()
+    val readTag = this.synchronized {
+      val readTag = nextReadTag
+      nextReadTag += 1
+      readPromises(readTag) = promise
+      readTag
+    }
+
+    logger.trace(s"Sending read request: $readTag")
+
+    sendEvent(EventFromValidator().withRead(
+      Read(
+        tag = readTag.toString,
+        keys = keys)))
+
+    promise
+      .future
+      .map { keyValuePairs =>
+        logger.trace(s"Key-value pairs received for $readTag")
+        val kvMap = keyValuePairs.map { pair => pair.key -> pair.value }.toMap
+        keys.map { key =>
+          val optValue = kvMap.get(key)
+          optValue.flatMap { v =>
+            if (v.isEmpty) None else Some(v)
+          }
+        }
+      }
+  }
+
+  override def write(data: Seq[(Key, Value, AccessControlList)]): Future[Unit] = Future {
+    implicit val ordering: Ordering[ByteString] = {
+      val comparator = ByteString.unsignedLexicographicalComparator()
+      (x, y) => comparator.compare(x, y)
+    }
+
+    val protectedKeyValuePairs =
+      data.sortBy(_._1).map { case (key, value, acl) =>
+        ProtectedKeyValuePair(key, value,
+          acl match {
+            case PublicAccess => Seq.empty
+            case RestrictedAccess(participants) =>
+              if (participants.isEmpty) Seq("_")
+              else participants.map(x => x: String).toSeq.sorted
+          })
+      }
+    sendEvent(EventFromValidator().withWrite(Write(protectedKeyValuePairs)))
+  }
+}
