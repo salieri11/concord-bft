db8055af9f3d7156c7e651162097815b84c799a8
Igor Golikov <igolikov@vmware.com>
2020-04-16 22:00:02 -0700

    KVB client fix

--- concord/src/consensus/kvb_client.cpp
+++ concord/src/consensus/kvb_client.cpp
@@ -53,6 +53,8 @@ bool KVBClient::send_request_sync(ConcordRequest &req, uint8_t flags,
   memset(m_outBuffer, 0, OUT_BUFFER_SIZE);
 
   uint32_t actualReplySize = 0;
+  LOG4CPLUS_DEBUG(logger_, "Invoking command with flags: "
+                               << flags << ", timeout: " << timeout.count());
   concordUtils::Status status = client_->invokeCommandSynch(
       command.c_str(), command.size(), flags, timeout, OUT_BUFFER_SIZE,
       m_outBuffer, &actualReplySize, correlation_id);
@@ -81,7 +83,6 @@ KVBClientPool::KVBClientPool(
       timeout_(timeout),
       clients_mutex_(),
       clients_condition_(),
-      wait_queue_(),
       shutdown_{false},
       kvbc_client_pool_requests_counters_(
           prometheus_registry->createCounterFamily(
@@ -136,19 +137,19 @@ bool KVBClientPool::send_request_sync(ConcordRequest &req, uint8_t flags,
   {
     std::unique_lock<std::mutex> clients_lock(clients_mutex_);
 
-    // Avoid starvation by forcing waiters to be unblocked in the order they
-    // started waiting.
-    std::thread::id my_thread_id = std::this_thread::get_id();
-    wait_queue_.push(my_thread_id);
-
-    auto predicate = [this, my_thread_id] {
-      // Only continue if either the node is shutting down, or if it's this
-      // thread's turn.
-      return this->shutdown_ ||
-             (my_thread_id == wait_queue_.front() && !clients_.empty());
+    auto predicate = [this] {
+      // Only continue if either the node is shutting down or there is at lease
+      // 1 available client in the pool
+      return this->shutdown_ || !clients_.empty();
     };
 
-    if (timeout > 0ms && timeout < std::chrono::milliseconds::max()) {
+    if (timeout > max_timeout_millis_) {
+      LOG4CPLUS_WARN(logger_, "Timeout: " << timeout.count()
+                                          << " > max_timeout_millis_: "
+                                          << max_timeout_millis_.count());
+      timeout = max_timeout_millis_;
+    }
+    if (timeout > 0ms) {
       if (!clients_condition_.wait_for(clients_lock, timeout, predicate)) {
         LOG4CPLUS_WARN(logger_, "Unable to claim a client in time.");
         ErrorResponse *err = resp.add_error_response();
@@ -161,40 +162,27 @@ bool KVBClientPool::send_request_sync(ConcordRequest &req, uint8_t flags,
     }
 
     if (shutdown_) {
-      // TODO: To make things super clean, we should find and remove ourselves
-      // from wait_queue_ as well, but if we're shutting down, we don't really
-      // care about that tracking.
       ErrorResponse *err = resp.add_error_response();
       err->set_description("Node is shutting down.");
       return true;
     }
 
-    wait_queue_.pop();
-
     client = clients_.front();
     clients_.pop();
-
-    if (!clients_.empty()) {
-      // We have to re-notify here, because it's possible that multiple notify
-      // calls happened before the head waiter woke up. In that case, the next
-      // waiter after this one may have woken, found that it was not next, and
-      // gone back to waiting. If there's a client for it, it needs to be woken
-      // again to grab it now.
-      clients_condition_.notify_all();
-    }
   }  // scope unlocks mutex
+
   kvbc_client_pool_received_requests_.Increment();
   bool result = client->send_request_sync(req, flags, timeout, parent_span,
                                           resp, correlation_id);
   kvbc_client_pool_received_replies_.Increment();
+
   {
     std::unique_lock<std::mutex> clients_lock(clients_mutex_);
     clients_.push(client);
-
-    // Wake all waiters, to be sure that the next one in the wait queue can grab
-    // a client.
-    clients_condition_.notify_all();
   }  // scope unlocks mutex
+
+  // one client returned to pool -> one thread needs to wake up
+  clients_condition_.notify_one();
 
   return result;
 }
