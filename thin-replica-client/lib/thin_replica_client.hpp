7b2ae46f850882b613cec8b0f69e06632e249925
Robert Muschner <rmuschner@vmware.com>
2020-05-19 19:40:47 +0000

    TRC: Timeout blocking Read() calls
    The TRC need f+1 replicas to reply with the same hash. While asking around not
    all replicas might be available due to the nature of a distributed system. The
    current Read() calls are blocking. To unlbock the TRC, this change wraps the
    blocking call into an asynchronous one with a timeout attached.

--- thin-replica-client/lib/thin_replica_client.hpp
+++ thin-replica-client/lib/thin_replica_client.hpp
@@ -47,8 +47,12 @@
 #include <prometheus/exposer.h>
 #include <prometheus/gauge.h>
 #include <prometheus/registry.h>
+#include <chrono>
 #include <condition_variable>
 #include <thread>
+
+// Short and readable time definitions, e.g: 5s
+using namespace std::chrono_literals;
 
 namespace thin_replica_client {
 
@@ -193,6 +197,10 @@ class ThinReplicaClient final {
   std::shared_ptr<UpdateQueue> update_queue_;
   uint16_t max_faulty_;
 
+  // Timeouts for data and hash stream read requests
+  std::chrono::milliseconds timeout_read_data_stream_;
+  std::chrono::milliseconds timeout_read_hash_stream_;
+
   // Condition variable to notify if an active subscription can no longer
   // progress.
   std::shared_ptr<std::condition_variable> subscription_failure_condition_;
@@ -267,6 +275,7 @@ class ThinReplicaClient final {
 
   void ResetDataStreamTo(size_t server_idx);
   void StartHashStreamWith(size_t server_idx);
+  void CloseAllHashStreams();
 
   // Helper functions to ReceiveUpdates.
   void RecordCollectedHash(
@@ -348,6 +357,8 @@ class ThinReplicaClient final {
         current_data_source_(0),
         subscription_hash_streams_(),
         sub_hash_contexts_(),
+        timeout_read_data_stream_(3s),
+        timeout_read_hash_stream_(3s),
         exposer_("0.0.0.0:9891", "/metrics", 1),
         registry_(std::make_shared<prometheus::Registry>()),
         trc_requests_counters_total_(
