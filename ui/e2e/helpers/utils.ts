c726934b2428c3be627dd385722db036b9dfbecf
Joe Kim <kkeunwoo@vmware.com>
2020-06-09 08:34:09 -0700

    UI: fix E2E test for newest CSP update
    - CSP decided to include input element in HTML while loading
    gif is going on. So we have to actually wait for the login element
    to become interactable.
    
    - Added `waitInteractableFor` in `ui/e2e/helpers/util.ts` to actually
    wait until element interactability.
    
    - Updated `login.po.ts` to use `waitInteractableFor` which will wait
    for target input elements (username/password) to be interactable.

--- ui/e2e/helpers/utils.ts
+++ ui/e2e/helpers/utils.ts
@@ -7,6 +7,15 @@ import { BROWSER_WAIT_TIME } from './constants';
 
 const until = protractor.ExpectedConditions;
 
+/**
+ * Actually wait for element by id to be interactable on the page;
+ * not just being present in HTML which can still be not interactable.
+ * @param cdd css locator pattern
+ */
+export function waitInteractableFor(css: string) {
+  browser.wait(until.elementToBeClickable(element(by.css(css))), BROWSER_WAIT_TIME);
+}
+
 export function waitFor(css: string) {
   browser.wait(until.presenceOf(element(by.css(css))), BROWSER_WAIT_TIME);
 }
