9a76e7e82dcd7fa570f491b2ba4f86dba01a3d74
Sanchita Roy <sanchitar@sanchitar-a02.vmware.com>
2020-06-11 13:13:56 -0700

    Set "-XX:+UseG1GC -Xmx10G" JVM parameters for dee

---
+++ persephone/config-service/src/test/java/com/vmware/blockchain/configuration/generateconfig/DamlExecutionEngineUtilTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019 VMware, Inc. All rights reserved. VMware Confidential
+ */
+
+package com.vmware.blockchain.configuration.generateconfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for DamlExecutionEngineUtil.
+ */
+public class DamlExecutionEngineUtilTest {
+
+    @Test
+    public void testHappyPath() throws IOException {
+
+        String actual = new DamlExecutionEngineUtil().generateConfig();
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("SampleDamlExecutionEngineConfig.txt").getFile());
+        var expected = new String(Files.readAllBytes(file.toPath()));
+
+        Assertions.assertThat(actual.equals(expected));
+    }
+
+    @Test
+    public void testPath() {
+        Assertions.assertThat(DamlExecutionEngineUtil.envVarPath.equals("/daml-execution-engine/environment-vars"))
+                .isTrue();
+    }
+}
