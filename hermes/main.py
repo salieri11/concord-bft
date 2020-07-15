ea4c7fe1021f901c2be3d93434f7dda7f051a195
Dmitry Danilov <ddanilov@vmware.com>
2020-07-15 06:16:06 -0700

    Hermes to use virtual env python

--- hermes/main.py
+++ hermes/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 #########################################################################
 # Copyright 2018 - 2020 VMware, Inc.  All rights reserved. -- VMware Confidential
