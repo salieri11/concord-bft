b08bdcebe6e720ce248d3fd71b09277daec2bedb
Ramkumar SG <rsg@vmware.com>
2020-08-19 17:58:10 -0700

    Get latest version/tag based on release directory

--- docker/get-build-info.py
+++ docker/get-build-info.py
@@ -2,12 +2,14 @@ import argparse
 import json
 import os
 import requests
+import sys
 
 ARTIFACTORY_BASE_URL = "https://build-artifactory.eng.vmware.com/api/storage/athena-docker-local/"
 ARTIFACTORY_MANIFEST_URL = ARTIFACTORY_BASE_URL + "{}/{}/manifest.json?properties"
-ARTIFACTORY_LASTMOD_URL = ARTIFACTORY_BASE_URL + "{}?lastModified"
+ARTIFACTORY_LASTMOD_URL = ARTIFACTORY_BASE_URL + "{}/{}?lastModified"
 KEY_HEADER = "X-JFrog-Art-Api"
 LATEST_TAG_FILE_PATH = os.getenv("WORKSPACE") + '/blockchain/vars/latest_tag.json' if os.getenv("WORKSPACE") else None
+BUILD_INFO_FILE = 'vars/build_info.json'
 
 def main():
     parser = argparse.ArgumentParser()
@@ -27,7 +29,8 @@ def main():
                         default="agent")
     cmdlineArgs = parser.parse_args()
     perform_precheck(cmdlineArgs)
-    buildData = retrieve_build_data(cmdlineArgs)
+    version_dir = retrieve_version_dir()
+    buildData = retrieve_build_data(cmdlineArgs, version_dir)
     display_data(cmdlineArgs, buildData)
 
 
@@ -37,24 +40,45 @@ def perform_precheck(cmdlineArgs):
         raise Exception("Set --artifactoryKey on the commnad line or set the " \
                         "ARTIFACTORY_KEY environment variable.")
 
+def get_build_info_file_path():
+   if os.getenv("WORKSPACE"):
+      build_info_file_path = os.path.join(os.getenv("WORKSPACE"), '/blockchain/', BUILD_INFO_FILE)
+   elif os.path.exists(BUILD_INFO_FILE):
+      build_info_file_path = BUILD_INFO_FILE
+   else:
+      print("Error: Execute script from blockchain/ directory")
+      sys.exit(1)
 
-def retrieve_build_data(cmdlineArgs):
+   return build_info_file_path
+
+def retrieve_version_dir():
+   build_info_file_path = get_build_info_file_path()
+   with open(build_info_file_path, "r") as fp:
+      data = json.load(fp)
+   build_numbers = data['build_numbers']
+   major_version = build_numbers["major"]
+   minor_version = build_numbers["minor"]
+   patch_version = build_numbers["patch"]
+   version_dir = "{}.{}.{}".format(major_version, minor_version, patch_version)
+   return version_dir
+
+def retrieve_build_data(cmdlineArgs, version_dir):
     api_key = get_artifactory_key(cmdlineArgs)
-    build = get_build(cmdlineArgs, api_key)
+    build = get_build(cmdlineArgs, api_key, version_dir)
     response = requests.get(ARTIFACTORY_MANIFEST_URL.format(cmdlineArgs.component, build),
                         headers={KEY_HEADER: api_key})
     check_response(cmdlineArgs, response)
     return json.loads(response.content.decode("UTF-8"))
 
 
-def get_build(cmdlineArgs, api_key):
+def get_build(cmdlineArgs, api_key, version_dir):
     if cmdlineArgs.build == "latest":
-        response = requests.get(ARTIFACTORY_LASTMOD_URL.format(cmdlineArgs.component),
+        response = requests.get(ARTIFACTORY_LASTMOD_URL.format(cmdlineArgs.component, version_dir),
                                 headers={KEY_HEADER: api_key})
         check_response(cmdlineArgs, response)
         content = json.loads(response.content.decode("UTF-8"))
         uri = content["uri"]
-        s = uri.split(cmdlineArgs.component + "/")[1]
+        s = uri.split(cmdlineArgs.component + "/{}/".format(version_dir))[1]
         buildNumber = s.split("/")[0]
         if LATEST_TAG_FILE_PATH:
           with open(LATEST_TAG_FILE_PATH, "w+") as f:
