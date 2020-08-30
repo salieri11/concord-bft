4ce1c38817c7e7d10167d58d170d09b21748415c
rtellamsetty <rtellamsetty@vmware.com>
2020-08-29 20:13:59 -0700

    Move all constants to a Config class.
    Provide a way to override these default values via a config properties file.
    
    A new config file 'api_test_config.properties' was introduced to override default properties.
    
    Sense the config file changes via a Watcherservice, and re-fill the config properties.
    
    Run the test cases in the purview of a Test Suite. Introduce this TestSuite class that sets up the stage for tests.

--- stark/api-tests/src/test/java/com/vmware/blockchain/api/test/base/ApiTestBase.java
+++ stark/api-tests/src/test/java/com/vmware/blockchain/api/test/base/ApiTestBase.java
@@ -12,84 +12,71 @@ import java.util.logging.Logger;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSession;
+import java.net.HttpURLConnection;
 
 import org.junit.BeforeClass;
 
 import com.google.gson.Gson;
 import com.vmware.blockchain.api.test.log.ApiLogger;
-
+import com.vmware.blockchain.api.test.config.ApiTestConfig;
+import com.vmware.blockchain.api.test.util.ConfigUtil;
 
 /**
  * Base class for API tests
  */
 public class ApiTestBase {
 
-    public static final String AUTH_HEADER_NAME = "csp-auth-token";
     public static Map<String, String> authMap;
 
     // Use the classname for the logger, this way you can refactor
-    private static Logger logger = null;
+    private static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     @BeforeClass
     /**
      * Initialize the required variables.
      */
     public static void init() {
-        setUpLogger();
         authenticate();
     }
-
-    /**
-     * Setup logger.
-     */
-    protected static void setUpLogger() {
-        try {
-            ApiLogger.setup();
-            logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Problem creating the log file.");
-        }
-    }
-
 
     /**
      * Authenticate with CSP and get an access token.
      */
     protected static void authenticate() {
+        // Get app config
+        ApiTestConfig apiTestConfig = ConfigUtil.getApiTestConfig();
+
         OutputStream out = null;
         InputStream inputStream = null;
-        HttpsURLConnection uc = null;
+        HttpsURLConnection cspConn = null;
         try {
-            URL url = new URL("https://console-stg.cloud.vmware.com/csp/gateway/am/api/auth/api-tokens/authorize");
-            logger.info("Trying to connect to " + url + "...");
+            URL cspUrl = new URL(apiTestConfig.getCspUrl());
+            logger.info("Trying to connect to " + cspUrl + "...");
 
-            uc = (HttpsURLConnection) url.openConnection();
-            logger.info("CONNECTED " + uc);
+            cspConn = (HttpsURLConnection) cspUrl.openConnection();
+            logger.info("Connected to CSP site " + cspConn);
             HostnameVerifier allHostsValid = new HostnameVerifier() {
                 public boolean verify(String hostname, SSLSession session) {
                     return true;
                 }
             };
-            uc.setHostnameVerifier(allHostsValid);
-            uc.setDoOutput(true);
-            byte[] data = "refresh_token=e0HWXg5LHxJnvZxgBuHM6rXyq5rNwCY5Ppohyz1lZZiqygt1eXtS9HwVn7nPs3Vv".getBytes();
-
+            cspConn.setHostnameVerifier(allHostsValid);
+            cspConn.setDoOutput(true);
+            byte[] data = (apiTestConfig.REFRESH_TOKEN + "=" + apiTestConfig.getCspRefreshToken()).getBytes();
             // Add headers
-            uc.setRequestMethod("POST");
-            uc.addRequestProperty("Accept", "application/json");
-            uc.addRequestProperty("Connection", "close");
-            uc.addRequestProperty("Content-Length", String.valueOf(data.length));
+            cspConn.setRequestMethod(apiTestConfig.HTTP_POST_PARAM);
+            cspConn.addRequestProperty(apiTestConfig.HTTP_ACCEPT_PARAM, apiTestConfig.HTTP_ACCEPT_TYPE_JSON);
+            cspConn.addRequestProperty(apiTestConfig.HTTP_CONNECTION_PARAM, apiTestConfig.HTTP_CONNECTION_CLOSE);
+            cspConn.addRequestProperty(apiTestConfig.HTTP_CONTENT_LEN_PARAM, String.valueOf(data.length));
 
-            out = uc.getOutputStream();
+            out = cspConn.getOutputStream();
             out.write(data);
             out.flush();
-            //out.close();
 
             StringBuilder builder = new StringBuilder();
-            if(uc.getResponseCode() == 200){
+            if(cspConn.getResponseCode() == 200){
                 if (inputStream == null) {
-                    inputStream = uc.getInputStream();
+                    inputStream = cspConn.getInputStream();
                 }
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
                 String line = null;
@@ -101,7 +88,6 @@ public class ApiTestBase {
             }
 
             logger.info("builder " + builder.toString());
-            //uc.disconnect();
 
             Gson gson = new Gson();
             authMap = (Map<String, String>)gson.fromJson(builder.toString(), Map.class);
@@ -109,15 +95,22 @@ public class ApiTestBase {
 
             String accessToken = authMap.get("access_token");
             logger.info("Token received = " + accessToken);
+
         } catch (java.net.MalformedURLException | java.net.ProtocolException e) {
             logger.severe("Unable to connect using the URL. Is the URL valid? " + e.getLocalizedMessage());
         } catch (java.io.IOException ioe) {
             logger.severe("Exception " + ioe.getLocalizedMessage());
         } finally {
             try {
-                out.close();
-                uc.disconnect();
-                inputStream.close();
+                if (out != null) {
+                    out.close();
+                }
+                if (cspConn != null) {
+                    cspConn.disconnect();
+                }
+                if (inputStream != null) {
+                    inputStream.close();
+                }
             } catch (IOException ioe) {
                 logger.severe(ioe.getLocalizedMessage());
             }
