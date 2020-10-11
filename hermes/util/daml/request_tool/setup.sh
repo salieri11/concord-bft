e48238516750c87ec40f5f8b83120e74c1c6644e
Robert Vollmar <rvollmar@vmware.com>
2020-10-11 15:16:23 -0700

    Change "host" to "ledger_host".

--- hermes/util/daml/request_tool/setup.sh
+++ hermes/util/daml/request_tool/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-host="${1}"
+ledger_host="${1}"
 
-if [ -z "${host}" ]
+if [ -z "${ledger_host}" ]
 then
-    host=localhost
+    ledger_host=localhost
 fi
 
 # This script is meant to ease test case development.
 # It builds the project, allocates parties, and uploads the dar, all
 # to the local sandbox ledger.
 daml build
-daml ledger allocate-parties --host "${host}" --port 6865
-daml ledger upload-dar --host "${host}" --port 6865 .daml/dist/ledgerclient-0.0.1.dar
+daml ledger allocate-parties --host "${ledger_host}" --port 6865
+daml ledger upload-dar --host "${ledger_host}" --port 6865 .daml/dist/ledgerclient-0.0.1.dar
