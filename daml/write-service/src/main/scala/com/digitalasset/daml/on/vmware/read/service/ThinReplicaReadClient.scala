35d0c66575ff197035e7e1d45e39e59843390bb7
Samir Talwar <samir.talwar@digitalasset.com>
2020-10-19 15:12:25 +0200

    DAML: Reformat ThinReplicaReadClient and fix some warnings.

--- daml/write-service/src/main/scala/com/digitalasset/daml/on/vmware/read/service/ThinReplicaReadClient.scala
+++ daml/write-service/src/main/scala/com/digitalasset/daml/on/vmware/read/service/ThinReplicaReadClient.scala
@@ -4,14 +4,15 @@ package com.digitalasset.daml.on.vmware.read.service
 
 import akka.NotUsed
 import akka.stream.scaladsl.{RestartSource, Source}
-import com.codahale.metrics.MetricRegistry
+import com.codahale.metrics.{MetricRegistry, Timer}
 import com.daml.metrics.VarGauge
 import com.digitalasset.daml.on.vmware.common.Constants
 import com.digitalasset.daml.on.vmware.thin.replica.client.core.{ThinReplicaClient, Update}
 import org.slf4j.LoggerFactory
+
 import scala.concurrent.duration._
 
-class ThinReplicaReadClient(
+final class ThinReplicaReadClient(
     clientId: String,
     maxFaulty: Short,
     privateKey: String,
@@ -20,19 +21,22 @@ class ThinReplicaReadClient(
     maxReadHashTimeout: Short,
     jaegerAgent: String,
     trcCore: ThinReplicaClient,
-    metrics: ThinReplicaReadClientMetrics) {
+    metrics: ThinReplicaReadClientMetrics,
+) {
+
   import ThinReplicaReadClient._
 
   private val logger = LoggerFactory.getLogger(this.getClass)
 
-  val trcCreated = trcCore.initialize(
+  private val trcCreated = trcCore.initialize(
     clientId,
     maxFaulty,
     privateKey,
     servers,
     maxReadDataTimeout,
     maxReadHashTimeout,
-    jaegerAgent)
+    jaegerAgent
+  )
   if (trcCreated)
     logger.info("Thin Replica Client created")
   else
@@ -60,7 +64,9 @@ class ThinReplicaReadClient(
       .unfoldResource[Block, Boolean](
         // open
         () => {
-          logger.info(s"Subscribing to `daml` key/value feed using thin replica, offset=$offset")
+          logger.info(
+            s"Subscribing to `daml` key/value feed using thin replica, offset=$offset"
+          )
           // If subscription is done for the first time, let the TRC
           // figure out the earliest available offset. After pruning
           // this offset may have quite high value.
@@ -75,13 +81,11 @@ class ThinReplicaReadClient(
           // None return signals end of resource
           if (subsResult)
             metrics.getBlockTimer
-              .time(() => trcCore.pop)
-              .map(
-                block => {
-                  metrics.lastBlockId.updateValue(block.blockId)
-                  block
-                }
-              )
+              .timeSupplier[Option[Update]](() => trcCore.pop())
+              .map(block => {
+                metrics.lastBlockId.updateValue(block.blockId)
+                block
+              })
           else
           None,
         // close
@@ -90,7 +94,9 @@ class ThinReplicaReadClient(
           if (!subsResult)
             logger.warn("Failed to subscribe to the thin replica, retrying...")
           else
-            logger.warn("Failed to pop an element from the thin replica, retrying...")
+            logger.warn(
+              "Failed to pop an element from the thin replica, retrying..."
+          )
       )
 }
 
@@ -99,8 +105,14 @@ object ThinReplicaReadClient {
 }
 
 class ThinReplicaReadClientMetrics(metricRegistry: MetricRegistry) {
-  val prefix = "daml.trc"
-  val getBlockTimer = metricRegistry.timer(s"$prefix.get-block")
-  val lastBlockId = new VarGauge[Long](0)
+
+  import ThinReplicaReadClientMetrics._
+
+  val getBlockTimer: Timer = metricRegistry.timer(s"$prefix.get-block")
+  val lastBlockId: VarGauge[Long] = new VarGauge[Long](0)
   metricRegistry.register(s"$prefix.last-block-id", lastBlockId)
+}
+
+object ThinReplicaReadClientMetrics {
+  private val prefix: String = "daml.trc"
 }
