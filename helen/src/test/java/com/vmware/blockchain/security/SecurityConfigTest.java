4b22ad545adf275d73466ec5725598eaed5e731f
machenbach <machenbach@vmware.com>
2020-10-19 16:05:07 -0700

    BC-4980: Missing Security Context
    Found by fortify scan.
    
    Added ContentSecurityPolicy in Security Config, and added unit test to look for this header
    
    Adding unit tests

---
+++ helen/src/test/java/com/vmware/blockchain/security/SecurityConfigTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2020 VMware, Inc. All rights reserved. VMware Confidential
+ */
+
+package com.vmware.blockchain.security;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import javax.servlet.Filter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.vmware.blockchain.BaseCacheHelper;
+import com.vmware.blockchain.MvcConfig;
+import com.vmware.blockchain.services.blockchains.BlockchainService;
+import com.vmware.blockchain.services.profiles.ConsortiumService;
+import com.vmware.blockchain.services.profiles.OrganizationService;
+import com.vmware.blockchain.services.profiles.UserService;
+
+/**
+ * SecurityConfigTest -- Make sure the security configuration is loaded, and that
+ * the Content-Security-Policy header is set.
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {CspSecurityConfig.class, MvcConfig.class})
+@TestPropertySource(locations = "classpath:test.properties", properties = "vmbc.auth.csp=true")
+@WebAppConfiguration
+public class SecurityConfigTest {
+
+    @MockBean
+    TokenAuthenticationConfig tokenAuthenticationConfig;
+
+    @MockBean
+    TokenAuthenticationProvider tokenAuthenticationProvider;
+
+    @MockBean
+    VmbcBasicAuthProvider vmbcBasicAuthProvider;
+
+    @MockBean
+    private RestAuthenticationEntryPoint restAuthticationEntryPoint;
+
+    @MockBean
+    private TokenRefreshFilter tokenRefreshFilter;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private BlockchainService blockchainService;
+
+    @MockBean
+    private ConsortiumService consortiumService;
+
+    @MockBean
+    private OrganizationService organizationService;
+
+    @MockBean
+    private BaseCacheHelper baseCacheHelper;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void init() throws Exception {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilters(springSecurityFilterChain)
+                .build();
+
+    }
+
+
+    @Test
+    // Make sure the Content-Security-Policy header is set.
+    void test1() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/users"))
+                .andReturn();
+        String securityPolicy = result.getResponse().getHeader("Content-Security-Policy");
+        Assertions.assertEquals("script-src 'self'", securityPolicy);
+    }
+}
