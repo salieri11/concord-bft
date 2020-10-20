ef978c3a9dff945d0d7ab7d01096d8e818bf0db6
Samir Talwar <samir.talwar@digitalasset.com>
2020-10-20 11:01:30 +0200

    DAML: Use the `HealthStatus` objects, rather than methods.

--- daml/common/src/main/scala/com/digitalasset/daml/on/vmware/common/ConvertHealth.scala
+++ daml/common/src/main/scala/com/digitalasset/daml/on/vmware/common/ConvertHealth.scala
@@ -1,12 +1,12 @@
 package com.digitalasset.daml.on.vmware.common
 
-import com.daml.ledger.api.health.HealthStatus
+import com.daml.ledger.api.health.{HealthStatus, Healthy, Unhealthy}
 
 object ConvertHealth {
   def fromNative(nativeHealth: Int): HealthStatus =
     nativeHealth match {
-      case 0 => HealthStatus.healthy
-      case 1 => HealthStatus.unhealthy
+      case 0 => Healthy
+      case 1 => Unhealthy
       case _ => sys.error(s"Unknown health status: $nativeHealth")
     }
 }
