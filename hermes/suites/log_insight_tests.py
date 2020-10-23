c8134850a23ec661e4a08e851275353c1ebb30b5
Sasi <subbiahs@vmware.com>
2020-10-23 15:15:43 +0530

    Added markers below describe

--- hermes/suites/log_insight_tests.py
+++ hermes/suites/log_insight_tests.py
@@ -56,8 +56,8 @@ def connect_to_log_insight(log_insight_host, username, password):
        return False, "Could not connect to LogInsight. Check error message for more details"
 
 
-@pytest.mark.smoke
 @describe("Verify Log Insight is connecting")
+@pytest.mark.smoke
 def test_log_insight_smoke():
     '''
     Verify Log Insight is connecting using below steps:
