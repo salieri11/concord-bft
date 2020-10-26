1f1877958fba52c11d786ec691dc2e28463725f5
Marcin Ziolek (c) <mziolek@vmware.com>
2020-10-26 12:19:23 -0700

    Adopt vDAML GCP scripts to Huntsman

--- daml/gcp/load-runner.sh
+++ daml/gcp/load-runner.sh
@@ -19,14 +19,29 @@ stop_prometheus () {
 }
 
 default_env_variables () {
-  export DEFAULT_SPIDER_IMAGE_TAG=1.30.477
-  export DEFAULT_DAML_SDK_VERSION=1.4.0
+  export DEFAULT_SPIDER_IMAGE_TAG=1.33.91
+  export DEFAULT_DAML_SDK_VERSION=1.6.0
   export DEFAULT_MARKET_FLAVOUR=sample
-  export DEFAULT_LOAD_RUNNER_TRADE_TIMEOUT=30
+  export DEFAULT_LOAD_RUNNER_RATE=20
   export DEFAULT_LOAD_RUNNER_CONCURRENCY=12
   export DEFAULT_LOAD_RUNNER_REQUESTS=2000
   export DEFAULT_NETTING_SIZE=100
-  export DEFAULT_USE_PREEXECUTION=false
+  export DEFAULT_USE_PREEXECUTION=true
+  export DEFAULT_SIMULATION_MODE=open
+}
+
+initialize_trade_source () {
+  case ${MARKET_FLAVOUR} in
+    sample)
+      export TRADE_SOURCE="data/sample/trades-20180912-WOW-10k.tsv.gzip"
+      ;;
+    nfr)
+      export TRADE_SOURCE="data/nfr/20191205_Trades.tsv.gzip"
+      ;;
+    storage)
+      export TRADE_SOURCE="data/storage/20200810_Trades.tsv.gzip"
+      ;;
+  esac
 }
 
 initialize_env_variables () {
@@ -35,11 +50,12 @@ initialize_env_variables () {
   export SPIDER_IMAGE_TAG=${SPIDER_IMAGE_TAG:-$DEFAULT_SPIDER_IMAGE_TAG}
   export DAML_SDK_VERSION=${DAML_SDK_VERSION:-$DEFAULT_DAML_SDK_VERSION}
   export MARKET_FLAVOUR=${MARKET_FLAVOUR:-$DEFAULT_MARKET_FLAVOUR}
-  export LOAD_RUNNER_TRADE_TIMEOUT=${LOAD_RUNNER_TRADE_TIMEOUT:-$DEFAULT_LOAD_RUNNER_TRADE_TIMEOUT}
+  export LOAD_RUNNER_RATE=${LOAD_RUNNER_RATE:-$DEFAULT_LOAD_RUNNER_RATE}
   export LOAD_RUNNER_CONCURRENCY=${LOAD_RUNNER_CONCURRENCY:-$DEFAULT_LOAD_RUNNER_CONCURRENCY}
   export LOAD_RUNNER_REQUESTS=${LOAD_RUNNER_REQUESTS:-$DEFAULT_LOAD_RUNNER_REQUESTS}
   export NETTING_SIZE=${NETTING_SIZE:-$DEFAULT_NETTING_SIZE}
   export USE_PREEXECUTION=${USE_PREEXECUTION:-$DEFAULT_USE_PREEXECUTION}
+  export SIMULATION_MODE=${SIMULATION_MODE:-$DEFAULT_SIMULATION_MODE}
   export DAY_T1=2018-09-13
   export DAY_T2=2018-09-14
   ## Set spider app metrics port to something that doesn't clash:
@@ -47,12 +63,14 @@ initialize_env_variables () {
   if [[ "$USE_PREEXECUTION" == "true" ]]; then
     export TEST_SUFFIX="-preexecution"
   fi
+  initialize_trade_source
 }
 
 load_docker_images() {
   docker pull digitalasset/spider-application:${SPIDER_IMAGE_TAG}
   docker pull digitalasset/spider-tools:${SPIDER_IMAGE_TAG}
   docker pull digitalasset/spider-load-runner:${SPIDER_IMAGE_TAG}
+  docker pull digitalasset/spider-compose:${SPIDER_IMAGE_TAG}
 }
 
 start_concord () {
@@ -70,110 +88,143 @@ stop_concord () {
   stop_network
 }
 
-locate_ledger_host () {
-  ## Populate ledger host variable
-  export LEDGER_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker_daml_ledger_api1_1`
-}
-
 import_spider_scripts () {
   ## Source the bash functions used in tests
-  source /dev/stdin <<< "$(docker run --rm digitalasset/spider-application:${SPIDER_IMAGE_TAG} vdaml-bootstrap)"
+  source /dev/stdin <<< "$(docker run --rm --entrypoint /render_bootstrap ${IMAGE_REGISTRY:-digitalasset}/spider-compose:${SPIDER_IMAGE_TAG})"
+}
+
+set_docker_network () {
+  _manage_override_value DOCKER_NETWORK_ENABLED true
+  _manage_override_value DOCKER_NETWORK_NAME vdaml_testing
+}
+
+locate_ledger_host () {
+  ## Populate ledger host variable
+  _manage_override_value LEDGER_HOST "$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker_daml_ledger_api1_1)"
+  _manage_override_value LEDGER_PORT 6865
 }
 
 install_dar () {
   ## Copy dar file out of spider-application container
-  extract-dar
+  extract_dar
   ## Upload dar file to ledger
-  upload-dar
+  upload_dar
   ## Stabilize the system after upload
   sleep 30
   ## Ensure dar file is loaded
-  warm-package
+  warm_package
 }
 
 start_spider () {
   ## Start spider application
-  export SPIDER_OPTS="--disable-auto-accept-proposals"
-  start-spider
+  spider up
 }
 
 stop_spider () {
-  stop-spider
+  spider down
+  docker ps -a --filter "name=spider_kafka" -q | xargs docker rm -f
 }
 
 create_prerequisites () {
   ## Create genesis contracts
-  market-genesis
-
+  spider_market_genesis
   ## Load sample data for load-runner
-  spider_proposals_off
-  import-market-set ${MARKET_FLAVOUR}
-  spider_proposals_on
+  spider_import_market_set ${MARKET_FLAVOUR}
 }
 
-trade_registration () {
-  ## Open the market
-  load-runner --simulation bmw.open-market
-  ## Run the benchmark tool
-  load-runner \
-        --simulation fix-trade.standard \
-        --trade-file /home/dlt/app/spider-load-tests/data/${MARKET_FLAVOUR}/fix_ae.tsv \
-        --loop-file \
-        --trade-timeout ${LOAD_RUNNER_TRADE_TIMEOUT}s \
-        --requests ${LOAD_RUNNER_REQUESTS} \
-        --concurrency ${LOAD_RUNNER_CONCURRENCY} \
-        --spec
+run_simulation () {
+
+  if [[ "$SIMULATION_MODE"=="open" ]]; then
+    SIMULATION_NAME=$1
+    MODE_SUFFIX="-o arrival.rate=${LOAD_RUNNER_RATE}"
+  else
+    SIMULATION_NAME=$2
+    MODE_SUFFIX="-o concurrent.ars=${LOAD_RUNNER_CONCURRENCY}"
+  fi
+
+  DAYS_SPEC=${@:3}
+  DURATION=$(( ${LOAD_RUNNER_REQUESTS} / ${LOAD_RUNNER_RATE} ))
+
+  kafka_ledger_bridge_up ${LOAD_RUNNER_CONCURRENCY}
+  gatling_simulations \
+    -s ${SIMULATION_NAME} \
+    -o duration="${DURATION} seconds" \
+    -o trades.source=${TRADE_SOURCE} \
+    ${MODE_SUFFIX} \
+    ${DAYS_SPEC}
+
 }
 
-netting () {
-  load-runner  --simulation bmw.close-market
-  load-runner  --simulation bmw.day-roll --date ${DAY_T1}
-  set-spider-batch-sizes ${NETTING_SIZE} ${NETTING_SIZE}
-  load-runner  --simulation bmw.begin-netting --bmw-timeout 4hour --concurrency 4
-}
-
-settlement () {
-  load-runner  --simulation bmw.day-roll --date  ${DAY_T2}
-  load-runner  --simulation bmw.settlement  --bmw-timeout 30min --concurrency 4
-}
-
-execute_all () {
-  while [[ $# -gt 0 ]]; do ($1); shift; done
+process_results () {
+  REQUEST_TYPE=${1:-"send trades (pick-to-AR)"}
+  HEADER_ECHO="===================== RESULTS: ${REQUEST_TYPE} ====================="
+  echo ${HEADER_ECHO}
+  TARGET="$(find results -name "*-202*" -and -type d | sort -r | head -n 1)"
+  if [[ -z "${TARGET}" ]]; then
+    MACHINE="$(hostname)"
+    csvjson "${TARGET}/summary.csv" | \
+      yq --yaml-output \
+        --arg machine ${MACHINE} \
+        --arg market_flavour ${MARKET_FLAVOUR} \
+        --arg request_type "${REQUEST_TYPE}" \
+        '. | map(select(.request == $request_type)) | first | {
+          "Machine": $machine,
+          "Ledger": "daml-on-vmware-concord",
+          "Market flavour": $market_flavour,
+          "Trades/second": .rps,
+          "Trades": .count,
+          "Duration": .duration,
+          "Latency": {
+            "min": .min,
+            "mean": .mean,
+            "avg": .avg,
+            "p90": .p90,
+            "p95": .p95,
+            "p99": .p99,
+            "p999": .p999,
+            "max": .max,
+            "stddev": .stddev,
+          }
+        }'
+  else
+    echo "no results were found"
+  fi
+  echo ${HEADER_ECHO} | sed -e 's/./=/g'
 }
 
 run_test () {
 
   echo "Launching load runner performance tool against DAML on VMware"
-  #set -x
-
-  initialize_env_variables
 
   load_docker_images
   start_concord
 
-  locate_ledger_host
   import_spider_scripts
+  set_docker_network
+  locate_ledger_host
 
   install_dar
-  start_spider
   create_prerequisites
+  start_spider
 
-  execute_all $@
+  run_simulation $@
+
+  process_results
 
   stop_spider
   stop_concord
 }
 
 stop_all () {
-  initialize_env_variables
   import_spider_scripts
+  set_docker_network
   stop_spider
+  kafka_ledger_bridge_down
   stop_concord
   stop_prometheus
 }
 
 print_help () {
-  default_env_variables
   echo -e "Usage: ./load-runner.sh COMMAND\n"
   echo "Available commands:"
   echo "help                     Show this help text"
@@ -190,25 +241,37 @@ print_help () {
   echo "DAML_SDK_VERSION  - test a specific DAML SDK version, default: $DEFAULT_DAML_SDK_VERSION"
   echo "MARKET_FLAVOUR    - select CHESS data flavor (sample, cde7), default: $DEFAULT_MARKET_FLAVOUR"
   echo "USE_PREEXECUTION  - set to true to turn on the preexecution, default: $DEFAULT_USE_PREEXECUTION"
-  echo "LOAD_RUNNER_TRADE_TIMEOUT    - define how long load-runner should wait for asynchronous responses, default: $DEFAULT_LOAD_RUNNER_TRADE_TIMEOUT"
+  echo "SIMULATION_MODE   - select the simulation mode (open, closed), default: $DEFAULT_SIMULATION_MODE"
+  echo "LOAD_RUNNER_RATE  - select the nominal trade rate, default: $DEFAULT_LOAD_RUNNER_RATE"
   echo "LOAD_RUNNER_CONCURRENCY      - define how many threads should load-runner use for communicating with the ledger, default: $DEFAULT_LOAD_RUNNER_CONCURRENCY"
   echo "LOAD_RUNNER_REQUESTS         - define how many trade registration requests should load-runner make, default: $DEFAULT_LOAD_RUNNER_REQUESTS"
 }
+
+initialize_env_variables
 
 OPERATION=${1:-"help"}
 
 case $OPERATION in
 
   trade)
-    run_test trade_registration
+    run_test \
+      "TradesAtFixedRate" \
+      "ClosedModelTrades"
     ;;
 
   net)
-    run_test trade_registration netting
+    run_test \
+      "SingleDayNetting" \
+      "ClosedModelSingleDayNetting" \
+      -o date.day2=${DAY_T1}
     ;;
 
   settle)
-    run_test trade_registration netting settlement
+    run_test \
+      "SingleDaySettlementInclNetting" \
+      "ClosedModelSingleDaySettlementInclNetting" \
+      -o date.day2=${DAY_T1} \
+      -o date.day3=${DAY_T2}
     ;;
 
   lprom)
