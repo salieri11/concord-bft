105269c79db8a2b8bba1154d31ee009e71dce621
Dinkar Rao <dinkarr@vmware.com>
2020-10-29 23:12:13 -0700

    BC-5383: Fix jsonschema problems with the latest SH ISO
    The new SH-ISO does not have the latest version of the jsonschema
    python package. It can only be updated by first installing pip, and
    then upgrading the jsonschema package.
    
    Fix the orchestrator build with this upgrade.

--- castor/common-appliance-platform/post-install-castor.sh
+++ castor/common-appliance-platform/post-install-castor.sh
@@ -80,6 +80,10 @@ rm python
 ln -s python3 python
 popd
 
+# Upgrade jsonschema to the latest version so Draft7Validator gets installed.
+# This is required for jsonschema to work with the Castor schemas.
+pip3 install --upgrade jsonschema
+
 # Install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod 755 /usr/local/bin/docker-compose
