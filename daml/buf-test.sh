e8e954d666155651876e9f72f8f2cb8154d92229
Samir Talwar <samir.talwar@digitalasset.com>
2020-11-19 16:46:54 +0100

    DAML: Run `buf check breaking` while building the execution engine.
    This will fail the Docker build if the protobuf has evolved in an
    incompatible way.

---
+++ daml/buf-test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+BUF_CONFIG='{"version": "v1beta1", "build": {"roots": ["concord/proto", "communication/src/main/proto"]}}'
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
+DAML_DIR="${ROOT_DIR}/daml"
+DAML_SDK_VERSION="$(cat "${DAML_DIR}/sdk.version")"
+DAML_INTEGRATION_KIT_VERSION="${DAML_SDK_VERSION}-$(cat "${DAML_DIR}/integration-kit-suffix.version")"
+
+PROTO_DIR="$(mktemp -d)"
+trap 'rm -rf "$PROTO_DIR"' EXIT
+
+cd "$PROTO_DIR"
+unzip "${DAML_DIR}/execution-engine/target/universal/stage/lib/com.digitalasset.daml.execution.engine.execution-engine-grpc-${DAML_INTEGRATION_KIT_VERSION}.jar" daml_validator.proto
+echo
+echo 'DAML proto files:'
+ls -lh
+
+cd "$ROOT_DIR"
+echo
+echo 'Buf configuration:'
+echo "${BUF_CONFIG}"
+echo
+echo 'Running `buf check breaking`...'
+buf --config="$BUF_CONFIG" check breaking --against="$PROTO_DIR"
+echo 'Check succeeded.'
