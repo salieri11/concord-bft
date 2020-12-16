8ce490330842ccf2f29a376e2b06c66ba1ed1fea
Nikolay Georgiev <ngeorgiev@vmware.com>
2020-12-16 04:21:18 -0800

    Implement & unit test DamlCommandsRecorder

---
+++ concord/src/daml/daml_commands_recorder.hpp
@@ -0,0 +1,44 @@
+// Copyright 2020 VMware, all rights reserved
+
+#ifndef CONCORD_DAML_COMMANDS_RECORDER_HPP_
+#define CONCORD_DAML_COMMANDS_RECORDER_HPP_
+
+#include <chrono>
+#include <map>
+
+#include "performance.pb.h"
+
+namespace concord {
+namespace daml {
+class DamlCommandsRecorder {
+ public:
+  DamlCommandsRecorder(const std::string& outputDir);
+
+  bool NewCommand(const std::string& cid);
+
+  const com::vmware::concord::performance::RecordedCommand& GetCommand(
+      const std::string& cid) const;
+
+  void AddCommandExecutionDuration(
+      std::string const& cid, std::chrono::steady_clock::duration duration);
+
+  void AddPreExecutionRead(const std::string& cid, std::string key,
+                           std::string value);
+  void AddPostExecutionRead(const std::string& cid, std::string key,
+                            std::string value);
+
+  void AddPostExecutionWrite(const std::string& cid, std::string key,
+                             std::string value);
+
+  bool SaveCommand(const std::string& cid);
+
+ private:
+  std::map<std::string, com::vmware::concord::performance::RecordedCommand>
+      recorded_commands_;
+  std::string output_dir_;
+};
+}  // namespace daml
+
+}  // namespace concord
+
+#endif  // CONCORD_DAML_COMMANDS_RECORDER_HPP_
